bca67543a9bdb01dd1c5c72fa5c60e361e3c1cf2
Adam Stephens <adam@valkor.net>
2013-07-06 17:20:40 -0400

    update and organize

--- shell/generic.sh
+++ shell/generic.sh
@@ -1,29 +1,37 @@
-#alias ll='ls -l'
+export CLICOLOR=''
+export PAGER='less -rFX'
+
+# _generic
 alias esl="exec $SHELL -l"
 alias pwgen='curl -k -3 https://mail.drh.net/cgi-bin/get_password.cgi'
 alias tree='tree -C'
-alias veewee='bundle exec veewee'
-alias vf='veewee fusion'
-alias vs='vagrant status'
-alias vv='veewee vbox'
 alias vundle_update='vim +BundleInstall +qall'
-alias synckvm='rsync -av ~/projects/greenarrow-integration/ kvm:/opt/greenarrow-integration/'
 
+# fresh
+source ~/.fresh/build/shell.sh
+
+# git
+alias gbv='git branch -av'
+alias gs='git status' 
+
+# rbenv
 if [ -d $HOME/.rbenv ]
 then
   export PATH="$PATH:$HOME/.rbenv/shims"
 fi
 if which rbenv > /dev/null 2>&1; then eval "$(rbenv init -)"; fi
 
-# fresh
-source ~/.fresh/build/shell.sh
+# ssh
+function delkey {
+  sed -i -e "/.*$1/d" ~/.ssh/known_hosts
+}
 
-export CLICOLOR=''
-export PAGER='less -rFX'
-
-
-#vagrant stuff
+# vagrant/veewee
 alias vdestroy='vagrant destroy '
+alias veewee='bundle exec veewee'
+alias vf='veewee fusion'
+alias vs='vagrant status'
+alias vv='veewee vbox'
 function vssh {
   vmName=$1
 
@@ -33,17 +41,8 @@ function vssh {
   fi
   vagrant ssh $vmName
 }
-
 function vreload {
   vmName=$1
   vagrant destroy -f $vmName
   vssh $vmName
 }
-
-function delkey {
-  sed -i -e "/.*$1/d" ~/.ssh/known_hosts
-}
-
-# my sublime text customizations
-#export EDITOR='subl -w'
-alias subl="'/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' "
