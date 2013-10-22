d03fa03826dea0d17af936c15776baba876d48f2
Adam Stephens <adam@valkor.net>
2013-10-22 15:37:17 -0400

    shell: package install all arguments

--- shell/packages.sh
+++ shell/packages.sh
@@ -30,13 +30,13 @@ function pki () {
   [ -x $1 ] && return 5
   case $DIST in
     'arch')
-      sudo pacman -S $1
+      sudo pacman -S $@
       ;;
     'debian')
-      sudo apt-get install $1
+      sudo apt-get install $@
       ;;
     'redhat')
-      sudo yum install $1
+      sudo yum install $@
       ;;
     *)
       return 6
