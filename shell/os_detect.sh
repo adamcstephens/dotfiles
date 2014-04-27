7cacd35aff13636c75e59b89fa264cbfcc016019
Adam Stephens <adam@valkor.net>
2014-04-27 18:16:03 -0400

    shell: do packages on all platforms, add freebsd

--- shell/os_detect.sh
+++ shell/os_detect.sh
@@ -1,11 +1,21 @@
-if which pacman > /dev/null 2>&1
-then
-  export DIST='arch' 
-elif which dpkg >/dev/null 2>&1
-then
-  export DIST='debian'
-elif which rpm > /dev/null 2>&1
-then
-  export DIST='redhat'
-fi
-
+OS=$(uname)
+case $OS in
+  'Linux')
+    if which pacman > /dev/null 2>&1
+    then
+      export DIST='arch' 
+    elif which dpkg >/dev/null 2>&1
+    then
+      export DIST='debian'
+    elif which rpm > /dev/null 2>&1
+    then
+      export DIST='redhat'
+    fi
+    ;;
+  'Darwin')
+    export DIST=$OS
+    ;;
+  'FreeBSD')
+    export DIST=$OS
+    ;;
+esac
