96db6ca1d063aa74113a25695abdacf1337072c7
Adam Stephens <adam.c.stephens@noaa.gov>
2014-08-18 09:24:19 -0400

    puppet-lint: --no-class_inherits_from_params_class-check

--- shell/puppet.sh
+++ shell/puppet.sh
@@ -1,4 +1,5 @@
 # puppet file
+alias pl='puppet-lint --no-80chars-check --no-class_inherits_from_params_class-check --with-filename'
 alias pp='puppet parser validate'
 function pes() {
   [ -z $1 ] && return 1
