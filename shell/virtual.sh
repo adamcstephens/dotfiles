34e9796f5e9d5058f5cec982b71a481eac566e05
Adam Stephens <adam.c.stephens@noaa.gov>
2014-09-30 08:36:10 -0400

    spaces in aliases allow for tab completion

--- shell/virtual.sh
+++ shell/virtual.sh
@@ -1,9 +1,9 @@
 # docker
-alias di='docker images'
-alias dk='docker kill'
-alias drm='docker rm'
-alias drmi='docker rmi'
-alias dps='docker ps'
+alias di='docker images '
+alias dk='docker kill '
+alias drm='docker rm '
+alias drmi='docker rmi '
+alias dps='docker ps '
 
 #
 # lxc
@@ -12,11 +12,11 @@ alias lxca='sudo lxc-ls --active'
 #
 # vagrant
 alias vdestroy='vagrant destroy '
-alias vp='vagrant provision'
-alias vs='vagrant status'
-alias vsg='vagrant global-status'
-alias vup='vagrant up'
-alias vssh='vagrant ssh'
+alias vp='vagrant provision '
+alias vs='vagrant status '
+alias vsg='vagrant global-status '
+alias vup='vagrant up '
+alias vssh='vagrant ssh '
 function vreload {
   vmName=$1
   vagrant destroy -f $vmName
