daca8f614b8f9a5a7cef7b36e0a3c24126ae4e10
Adam Stephens <adam.c.stephens@noaa.gov>
2014-10-24 16:38:26 -0400

    puppet-lint: --no-variable_scope-check

--- shell/puppet.sh
+++ shell/puppet.sh
@@ -1,5 +1,5 @@
 # puppet file
-alias pl='puppet-lint --no-80chars-check --no-class_inherits_from_params_class-check --with-filename'
+alias pl='puppet-lint --no-80chars-check --no-class_inherits_from_params_class-check --no-variable_scope-check --with-filename'
 alias pp='puppet parser validate'
 function pes() {
   [ -z $1 ] && return 1
