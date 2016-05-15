953ebd3c3d372043fec7757cb814a36eb763def4
Adam Stephens <adam@boxy.boko.private>
2016-05-15 11:28:32 -0400

    add fedora support

---
+++ shell/os/Linux/fedora.sh
@@ -0,0 +1,12 @@
+# support newer grep
+alias grep='grep --color=auto'
+unset GREP_OPTIONS
+
+# packages
+alias pki="sudo dnf --color=auto install"
+alias pkls="rpm -ql"
+alias pkp="dnf --color=auto provides"
+alias pks="dnf --color=auto search"
+alias pksh="dnf --color=auto info"
+alias pku="sudo dnf --color=auto update"
+alias pkr="sudo dnf --color=auto remove"
