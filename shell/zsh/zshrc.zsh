3ddd132c960638dcb89e3e6fcfde0e1ea9394b72
Adam Stephens <astephens@covermymeds.com>
2016-07-21 15:45:33 -0400

    bah stupid bindkeys from 2014!

--- shell/zsh/zshrc.zsh
+++ shell/zsh/zshrc.zsh
@@ -68,12 +68,6 @@ key[PageDown]=${terminfo[knp]}
 [[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"   beginning-of-buffer-or-history
 [[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}" end-of-buffer-or-history
 
-# fix some navigation
-bindkey '3D' emacs-backward-word
-bindkey '3C' emacs-forward-word
-bindkey '5D' emacs-backward-word
-bindkey '5C' emacs-forward-word
-
 # Finally, make sure the terminal is in application mode, when zle is
 # active. Only then are the values from $terminfo valid.
 if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
