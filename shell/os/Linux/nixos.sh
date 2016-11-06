7b272c3bffabb9fe702a20f9dd591b93ef4c13b1
Adam Stephens <adam@valkor.net>
2016-11-06 16:33:19 -0500

    add nixos and fix the shell loading

---
+++ shell/os/Linux/nixos.sh
@@ -0,0 +1,7 @@
+#alias pki="sudo apt-get install"
+#alias pkls="dpkg -L"
+#alias pkp="apt-file search"
+alias pks="nix-env -qa "
+#alias pksh="apt-cache show"
+#alias pku="sudo apt-get update && sudo apt-get dist-upgrade"
+#alias pkr="sudo apt-get remove"
