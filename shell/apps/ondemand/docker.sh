0e60abd12f59b5d7ef1feaa79f1e5c1af47f95e2
Adam Stephens <astephens@covermymeds.com>
2016-11-17 12:51:15 -0500

    docker should be detected

---
+++ shell/apps/ondemand/docker.sh
@@ -0,0 +1,19 @@
+# docker
+alias di='docker images '
+alias dk='docker kill '
+alias drm='docker rm '
+alias drmi='docker rmi '
+alias dps='docker ps '
+function dsh {
+  [ -z $1 ] && echo "needs image to run." && return 2
+  runimg="$1"
+  shift
+  if [ -z $1 ]
+  then
+    runcmd="/bin/bash -l"
+  else
+    runcmd="$@"
+  fi
+  docker run -t -i --rm=true $runimg $runcmd
+}
+alias docker_rm_all="docker ps -a | grep -vi container | awk '{print $1}' | xargs docker rm -f"
--- shell/virtual.sh
+++ shell/virtual.sh
@@ -1,15 +1,3 @@
-# docker
-alias di='docker images '
-alias dk='docker kill '
-alias drm='docker rm '
-alias drmi='docker rmi '
-alias dps='docker ps '
-function dsh {
-  [ -z $1 ] && echo "needs image to run." && return 2
-  docker run -t -i --rm=true $1 /bin/bash -l
-}
-alias docker_rm_all="docker ps -a | grep -vi container | awk '{print $1}' | xargs docker rm -f"
-
 #
 # lxc
 alias lxca='sudo lxc-ls --active'
