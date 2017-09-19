a59f2a8c4c08508ab03b6d67f5524983d831c211
Adam Stephens <astephens@covermymeds.com>
2017-09-19 13:48:12 -0400

    just take a stab at using zplug

--- shell/zsh/fresh.zsh
+++
@@ -1,5 +0,0 @@
-# follow fresh recommended  https://github.com/freshshell/fresh/wiki/Loading-Zsh-Completions
-if [ -d ~/.fresh/build/completion ]
-then
-  fpath=(~/.fresh/build/completion $fpath)
-fi
