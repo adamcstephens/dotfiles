0a5b308adf7f63a5f41725c421b56ab9b9c02a65
Adam Stephens <astephens@covermymeds.com>
2018-08-02 10:03:23 -0400

    add a default to finicky and some comments

--- config/finicky.js
+++ config/finicky.js
@@ -1,9 +1,14 @@
+// default is chrome which we never want
+finicky.setDefaultBrowser('com.apple.Safari');
+
 finicky.onUrl(function(url, opts) {
   if (url.match(/^https?:\/\/portal\.azure\.com/)) {
     return {
       bundleIdentifier: "com.google.Chrome"
     };
+
   } else if (url.match(/^https?:\/\/bluejeans\.com/)) {
+    // modify the bluejeans URL for what the app wants
     var url = url.replace(
       /^https?:\/\/bluejeans\.com/,
       "bjnb://meet/id"
@@ -12,11 +17,14 @@ finicky.onUrl(function(url, opts) {
       bundleIdentifier: "com.bluejeansnet.Blue",
       url: url
     };
+
   } else if (url.match(/^https?:\/\/open\.spotify\.com/)) {
     return {
       bundleIdentifier: "com.spotify.client"
     };
+
   } else {
+    // if the default browser isn't open, choose an open one
     return {
       bundleIdentifier: [
         "com.apple.Safari",
