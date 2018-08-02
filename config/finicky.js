6007be54f4d42573ec1cb9c4886006c54581ebfa
Adam Stephens <astephens@covermymeds.com>
2018-08-02 09:57:36 -0400

    yay finicky rules are working now!

--- config/finicky.js
+++ config/finicky.js
@@ -1,15 +1,28 @@
 finicky.onUrl(function(url, opts) {
-  if (url.match(/^https?:\/\/.*azure\.portal\.com.*/)) {
+  if (url.match(/^https?:\/\/portal\.azure\.com/)) {
     return {
       bundleIdentifier: "com.google.Chrome"
     };
+  } else if (url.match(/^https?:\/\/bluejeans\.com/)) {
+    var url = url.replace(
+      /^https?:\/\/bluejeans\.com/,
+      "bjnb://meet/id"
+    );
+    return {
+      bundleIdentifier: "com.bluejeansnet.Blue",
+      url: url
+    };
+  } else if (url.match(/^https?:\/\/open\.spotify\.com/)) {
+    return {
+      bundleIdentifier: "com.spotify.client"
+    };
+  } else {
+    return {
+      bundleIdentifier: [
+        "com.apple.Safari",
+        "org.mozilla.firefox",
+        "com.google.Chrome"
+      ]
+    };
   }
-
-  return {
-    bundleIdentifier: [
-      "com.apple.Safari",
-      "org.mozilla.firefox",
-      "com.google.Chrome"
-    ]
-  };
 });
