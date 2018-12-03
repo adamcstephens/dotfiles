229ed831e0c273adadb5521bef669230dd362cf9
Adam Stephens <astephens@covermymeds.com>
2018-12-03 11:07:16 -0500

    add zsh autosuggestion

--- shell/zsh/antibody.zsh
+++ shell/zsh/antibody.zsh
@@ -1,1 +1,6 @@
 source ~/.zsh_plugins.sh
+
+# zsh-users/zsh-autosuggestions settings
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=100
+export ZSH_AUTOSUGGEST_USE_ASYNC=''
+bindkey '^ ' autosuggest-accept
