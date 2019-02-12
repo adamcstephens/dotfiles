408454e969bede7196332488cd5c6f3d1ddedd30
Adam Stephens <astephens@covermymeds.com>
2019-02-12 09:35:09 -0500

    swap zsh syntax highlighting plugin

--- shell/zsh/zplug.zsh
+++ shell/zsh/zplug.zsh
@@ -6,12 +6,13 @@ zplug "zsh-users/zsh-autosuggestions"
 # zplug "plugins/docker",         from:oh-my-zsh
 # zplug "plugins/docker-compose", from:oh-my-zsh, as:plugin
 zplug "~/.fresh/source/freshshell/fresh/contrib/completion/fresh-completion.zsh", from:local
+zplug "kiurchv/asdf.plugin.zsh", defer:2
 
 # self-manage zplug
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 # this is loaded last
-zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zdharma/fast-syntax-highlighting"
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
