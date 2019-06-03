cc6797caade93b2e62a2511c3ad673ba8b0c7e69
Adam Stephens <astephens@covermymeds.com>
2019-06-03 17:55:40 -0400

    update finicky to v2 config

--- config/finicky.js
+++ config/finicky.js
@@ -1,37 +1,31 @@
-// default is chrome which we never want
-finicky.setDefaultBrowser('org.mozilla.firefox');
-
-finicky.onUrl(function(url, opts) {
-  if (url.match(/^https?:\/\/([a-z]+\.)?bluejeans\.com\/[0-9]+/)) {
-    // modify the bluejeans URL for what the app wants
-    var url = url.replace(
-      /^https?:\/\/([a-z]+\.)?bluejeans\.com/,
-      "bjnb://meet/id"
-    );
-    return {
-      bundleIdentifier: "com.bluejeansnet.Blue",
-      url: url
-    };
-
-  } else if (url.match(/^https:\/\/insiders.liveshare.vsengsaas.visualstudio.com\/join\?[A-Z0-9]$/)) {
-    return {
-      bundleIdentifier: "com.microsoft.VSCode"
-    };
-
-
-  } else if (url.match(/^https?:\/\/open\.spotify\.com/)) {
-    return {
-      bundleIdentifier: "com.spotify.client"
-    };
-
-  } else {
-    // if the default browser isn't open, choose an open one
-    return {
-      bundleIdentifier: [
-        "org.mozilla.firefox",
-        "com.apple.Safari",
-        "com.google.Chrome"
-      ]
-    };
-  }
-});
+module.exports = {
+  defaultBrowser: "Firefox",
+  rewrite: [
+    {
+      match: /^https?:\/\/([a-z]+\.)?bluejeans\.com\/[0-9]+/,
+      url({ url }) {
+        var path = url.pathname.replace(/([0-9]+)/, "id/$1");
+        return {
+          ...url,
+          host: "meet",
+          pathname: path,
+          protocol: "bjnb"
+        }
+      }
+    }
+  ],
+  handlers: [
+    {
+      match: /^https:\/\/insiders.liveshare.vsengsaas.visualstudio.com\/join\?[A-Z0-9]$/,
+      browser: "Visual Studio Code"
+    },
+    {
+      match: /bjnb:\/\/meet\/id/,
+      browser: "BlueJeans"
+    },
+    {
+      match: finicky.matchDomains("open.spotify.com"),
+      browser: "Spotify"
+    }
+  ]
+};
