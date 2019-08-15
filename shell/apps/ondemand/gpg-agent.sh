01362db7f1513f7ebd78a3e997c06965c61bb26b
Adam Stephens <astephens@covermymeds.com>
2019-08-15 10:53:53 -0400

    tweaks some configs

--- shell/apps/ondemand/gpg-agent.sh
+++ shell/apps/ondemand/gpg-agent.sh
@@ -8,3 +8,12 @@ then
   # gpg-agent to provide for ssh too
   export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 fi
+
+# if [ -f "${HOME}/.gpg-agent-info" ]; then
+#      source "${HOME}/.gpg-agent-info"
+#        export GPG_AGENT_INFO
+#        export SSH_AUTH_SOCK
+#        export SSH_AGENT_PID
+# else
+#     eval $( gpg-agent --daemon --write-env-file ~/.gpg-agent-info )
+# fi<Paste>
