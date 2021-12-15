6fbd414ab9f526f4761d5e47bc75aa5c05a65ea3
Adam Stephens <adam@valkor.net>
2021-12-15 10:59:04 -0500

    improve delkey function

--- fish/functions/delkey.fish
+++ fish/functions/delkey.fish
@@ -1,9 +1,13 @@
 function delkey
-    set -l target $argv[1]
+    if [ (count $argv) = 0 ]
+        echo "supply deletion key"
+        return 2
+    end
 
-    [ -z $target ] && echo "supply deletion key" && return 2
-    set -l delip (grep "$target " ~/.ssh/known_hosts | awk '{print $1}' | cut -f 2 -d \, | uniq)
-    # breakpoint
-    [ -z $delip ] || ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$delip"
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$target"
+    for target in $argv
+        set -l delip (grep "$target " ~/.ssh/known_hosts | awk '{print $1}' | cut -f 2 -d \, | uniq)
+        # breakpoint
+        [ -z $delip ] || ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$delip"
+        ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$target"
+    end
 end
