1a6ed84ca71ddff857f01541adb37677737d9157
Adam Stephens <adam@valkor.net>
2021-12-21 10:52:27 -0500

    install asdf direnv, and use brew for python/node

--- fish/conf.d/brew.fish
+++ fish/conf.d/brew.fish
@@ -1,3 +1,11 @@
 if test -x /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
+
+    if test -d /opt/homebrew/opt/node@16/bin
+        fish_add_path --prepend /opt/homebrew/opt/node@16/bin
+    end
+
+    if test -d /opt/homebrew/opt/python@3.10/bin
+        fish_add_path --prepend /opt/homebrew/opt/python@3.10/bin
+    end
 end
