0c15e131a9c81dc8262df602853b2e72e7cfe0f8
Adam Stephens <adam@valkor.net>
2022-01-22 14:21:37 -0500

    fish: simplifiy gnome keyring loading

--- fish/conf.d/keyring.fish
+++ fish/conf.d/keyring.fish
@@ -1,5 +1,3 @@
 if status is-interactive && command -v gnome-keyring-daemon &>/dev/null
-    gnome-keyring-daemon --start --components=pkcs11,secrets,ssh 2>/dev/null | read --line ssh_auth_sock
-
-    set -gx SSH_AUTH_SOCK (string split -m 1 = $ssh_auth_sock)[2]
+    set -x (gnome-keyring-daemon --start 2>/dev/null | string split "=")
 end
