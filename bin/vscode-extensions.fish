1cf5a58def78e85aa1f4ebf49e0a7ac058b9a30a
Adam Stephens <adam@valkor.net>
2022-01-26 13:55:57 -0500

    vscode: better uninstall handling...?

--- bin/vscode-extensions.fish
+++ bin/vscode-extensions.fish
@@ -1,15 +1,24 @@
 #!/usr/bin/env fish
 
 set extfile ~/.dotfiles/vscode/extensions.txt
+set uninstfile ~/.dotfiles/vscode/extensions-uninstall.txt
 
 if [ "$argv[1]" = remove ]
     sed -i "/$argv[2]/d" $extfile
     code --uninstall-extension $argv[2]
+    echo $argv[2] >>$uninstfile
     exit $status
 end
 
 set installed (code --list-extensions)
 set repo (cat $extfile)
+set uninstall (cat $uninstfile)
+
+for u in $uninstall
+    if contains $u $installed
+        code --uninstall-extension $u
+    end
+end
 
 for e in $repo
     if not contains $e $installed
