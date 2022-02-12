473b5c43471abb4f63d8a068f93b2371775f6254
Adam Stephens <adam@valkor.net>
2022-02-12 16:02:24 -0500

    think updates

--- fish/functions/emacsclient.fish
+++ fish/functions/emacsclient.fish
@@ -1,8 +1,10 @@
 function emacsclient
+    set OLDTERM $TERM
     if [ -e ~/.terminfo/x/xterm-emacs ]
-        set -x TERM xterm-emacs
+        set TERM xterm-emacs
     else
-        set -x TERM xterm-256color
+        set TERM xterm-256color
     end
     command emacsclient -t $argv
+    set TERM $OLDTERM
 end
--- river.sh
+++ river.sh
@@ -5,123 +5,118 @@ riverctl background-color 0x1c1d21
 riverctl border-color-focused 0xcbcdd2
 riverctl border-color-unfocused 0x3f4044
 
-# Use the "logo" key as the primary modifier
-mod="Mod4"
-
 # Mod+Shift+Return to start an instance of foot (https://codeberg.org/dnkl/foot)
-riverctl map normal $mod Return spawn kitty
-riverctl map normal $mod+Shift T spawn kitty
-riverctl map normal $mod+Shift W spawn 'gtk-launch firefox'
-riverctl map normal $mod+Shift V spawn 'gtk-launch code'
-riverctl map normal $mod+Shift+Control E spawn 'gtk-launch emacsclient'
-riverctl map normal $mod D spawn 'wofi --show drun,run'
+riverctl map normal Mod4+Shift T spawn kitty
+riverctl map normal Mod4+Shift W spawn 'gtk-launch firefox'
+riverctl map normal Mod4+Shift V spawn 'gtk-launch code'
+riverctl map normal Mod4+Shift+Control E spawn 'gtk-launch emacsclient'
+riverctl map normal Mod4 D spawn 'wofi --show drun,run'
 
 # Mod+Q to close the focused view
-riverctl map normal $mod Q close
+riverctl map normal Mod4+Shift Q close
 
 # Mod+E to exit river
-riverctl map normal $mod+Shift E exit
+riverctl map normal Mod4+Shift E exit
 
 # Mod+J and Mod+K to focus the next/previous view in the layout stack
-riverctl map normal $mod J focus-view next
-riverctl map normal $mod K focus-view previous
-riverctl map normal $mod S focus-view next
-riverctl map normal $mod W focus-view previous
+riverctl map normal Mod4 J focus-view next
+riverctl map normal Mod4 K focus-view previous
+riverctl map normal Mod4 S focus-view next
+riverctl map normal Mod4 W focus-view previous
 
 # Mod+Shift+J and Mod+Shift+K to swap the focused view with the next/previous
 # view in the layout stack
-riverctl map normal $mod+Shift J swap next
-riverctl map normal $mod+Shift K swap previous
-riverctl map normal $mod+Shift S swap next
-riverctl map normal $mod+Shift W swap previous
+riverctl map normal Mod4+Shift J swap next
+riverctl map normal Mod4+Shift K swap previous
+riverctl map normal Mod4+Shift S swap next
+riverctl map normal Mod4+Shift W swap previous
 
 # Mod+Period and Mod+Comma to focus the next/previous output
-riverctl map normal $mod Period focus-output next
-riverctl map normal $mod Comma focus-output previous
+riverctl map normal Mod4 Period focus-output next
+riverctl map normal Mod4 Comma focus-output previous
 
 # Mod+Shift+{Period,Comma} to send the focused view to the next/previous output
-riverctl map normal $mod+Shift Period send-to-output next
-riverctl map normal $mod+Shift Comma send-to-output previous
+riverctl map normal Mod4+Shift Period send-to-output next
+riverctl map normal Mod4+Shift Comma send-to-output previous
 
 # Mod+Return to bump the focused view to the top of the layout stack
-riverctl map normal $mod+Shift Return zoom
-riverctl map normal $mod A zoom
+riverctl map normal Mod4 A zoom
 
 # Mod+H and Mod+L to decrease/increase the main ratio of rivertile(1)
-riverctl map normal $mod H send-layout-cmd rivertile "main-ratio -0.05"
-riverctl map normal $mod L send-layout-cmd rivertile "main-ratio +0.05"
+riverctl map normal Mod4 H send-layout-cmd rivertile "main-ratio -0.05"
+riverctl map normal Mod4 L send-layout-cmd rivertile "main-ratio +0.05"
 
 # Mod+Shift+H and Mod+Shift+L to increment/decrement the main count of rivertile(1)
-riverctl map normal $mod+Shift H send-layout-cmd rivertile "main-count +1"
-riverctl map normal $mod+Shift L send-layout-cmd rivertile "main-count -1"
+riverctl map normal Mod4+Shift H send-layout-cmd rivertile "main-count +1"
+riverctl map normal Mod4+Shift L send-layout-cmd rivertile "main-count -1"
 
 # Mod+Alt+{H,J,K,L} to move views
-riverctl map normal $mod+Mod1 H move left 100
-riverctl map normal $mod+Mod1 J move down 100
-riverctl map normal $mod+Mod1 K move up 100
-riverctl map normal $mod+Mod1 L move right 100
+riverctl map normal Mod4+Mod1 H move left 100
+riverctl map normal Mod4+Mod1 J move down 100
+riverctl map normal Mod4+Mod1 K move up 100
+riverctl map normal Mod4+Mod1 L move right 100
 
 # Mod+Alt+Control+{H,J,K,L} to snap views to screen edges
-riverctl map normal $mod+Mod1+Control H snap left
-riverctl map normal $mod+Mod1+Control J snap down
-riverctl map normal $mod+Mod1+Control K snap up
-riverctl map normal $mod+Mod1+Control L snap right
+riverctl map normal Mod4+Mod1+Control H snap left
+riverctl map normal Mod4+Mod1+Control J snap down
+riverctl map normal Mod4+Mod1+Control K snap up
+riverctl map normal Mod4+Mod1+Control L snap right
 
 # Mod+Alt+Shif+{H,J,K,L} to resize views
-riverctl map normal $mod+Mod1+Shift H resize horizontal -100
-riverctl map normal $mod+Mod1+Shift J resize vertical 100
-riverctl map normal $mod+Mod1+Shift K resize vertical -100
-riverctl map normal $mod+Mod1+Shift L resize horizontal 100
+riverctl map normal Mod4+Mod1+Shift H resize horizontal -100
+riverctl map normal Mod4+Mod1+Shift J resize vertical 100
+riverctl map normal Mod4+Mod1+Shift K resize vertical -100
+riverctl map normal Mod4+Mod1+Shift L resize horizontal 100
 
 # Mod + Left Mouse Button to move views
-# riverctl map-pointer normal $mod BTN_LEFT move-view
+# riverctl map-pointer normal Mod4 BTN_LEFT move-view
 
 # Mod + Right Mouse Button to resize views
-# riverctl map-pointer normal $mod BTN_RIGHT resize-view
+# riverctl map-pointer normal Mod4 BTN_RIGHT resize-view
 
 for i in $(seq 1 9); do
   tags=$((1 << ($i - 1)))
 
   # Mod+[1-9] to focus tag [0-8]
-  riverctl map normal $mod $i set-focused-tags $tags
+  riverctl map normal Mod4 $i set-focused-tags $tags
 
   # Mod+Shift+[1-9] to tag focused view with tag [0-8]
-  riverctl map normal $mod+Shift $i set-view-tags $tags
+  riverctl map normal Mod4+Shift $i set-view-tags $tags
 
   # Mod+Ctrl+[1-9] to toggle focus of tag [0-8]
-  riverctl map normal $mod+Control $i toggle-focused-tags $tags
+  riverctl map normal Mod4+Control $i toggle-focused-tags $tags
 
   # Mod+Shift+Ctrl+[1-9] to toggle tag [0-8] of focused view
-  riverctl map normal $mod+Shift+Control $i toggle-view-tags $tags
+  riverctl map normal Mod4+Shift+Control $i toggle-view-tags $tags
 done
 
 # Mod+0 to focus all tags
 # Mod+Shift+0 to tag focused view with all tags
 all_tags=$(((1 << 32) - 1))
-riverctl map normal $mod 0 set-focused-tags $all_tags
-riverctl map normal $mod+Shift 0 set-view-tags $all_tags
+riverctl map normal Mod4 0 set-focused-tags $all_tags
+riverctl map normal Mod4+Shift 0 set-view-tags $all_tags
 
 # Mod+Space to toggle float
-riverctl map normal $mod+Shift Space toggle-float
+riverctl map normal Mod4+Shift Space toggle-float
 
 # Mod+F to toggle fullscreen
-riverctl map normal $mod F toggle-fullscreen
+riverctl map normal Mod4 F toggle-fullscreen
 
 # Mod+{Up,Right,Down,Left} to change layout orientation
-riverctl map normal $mod Up send-layout-cmd rivertile "main-location top"
-riverctl map normal $mod Right send-layout-cmd rivertile "main-location right"
-riverctl map normal $mod Down send-layout-cmd rivertile "main-location bottom"
-riverctl map normal $mod Left send-layout-cmd rivertile "main-location left"
+riverctl map normal Mod4 Up send-layout-cmd rivertile "main-location top"
+riverctl map normal Mod4 Right send-layout-cmd rivertile "main-location right"
+riverctl map normal Mod4 Down send-layout-cmd rivertile "main-location bottom"
+riverctl map normal Mod4 Left send-layout-cmd rivertile "main-location left"
 
 # Declare a passthrough mode. This mode has only a single mapping to return to
 # normal mode. This makes it useful for testing a nested wayland compositor
 riverctl declare-mode passthrough
 
 # Mod+F11 to enter passthrough mode
-riverctl map normal $mod F11 enter-mode passthrough
+riverctl map normal Mod4 F11 enter-mode passthrough
 
 # Mod+F11 to return to normal mode
-riverctl map passthrough $mod F11 enter-mode normal
+riverctl map passthrough Mod4 F11 enter-mode normal
 
 # Various media key mapping examples for both normal and locked mode which do
 # not have a modifier
