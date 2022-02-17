324bc6d6c01b64845dc2ab18bbd1893e7e51f21d
Adam Stephens <adam@valkor.net>
2022-02-17 15:44:32 -0500

    update vscode settings

--- bin/vscode-extensions.fish
+++ bin/vscode-extensions.fish
@@ -3,7 +3,10 @@
 set extfile ~/.dotfiles/vscode/extensions.txt
 set uninstfile ~/.dotfiles/vscode/extensions-uninstall.txt
 
-if [ "$argv[1]" = remove ]
+if [ "$argv[1]" = install ]
+    sed -i "/$argv[2]/d" $uninstfile
+    code --install-extension $argv[2]
+else if [ "$argv[1]" = remove ]
     sed -i "/$argv[2]/d" $extfile
     code --uninstall-extension $argv[2]
     echo $argv[2] >>$uninstfile
