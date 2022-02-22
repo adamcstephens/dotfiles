899df5282e438458ef2ae40b384de51578937384
Adam Stephens <adam@valkor.net>
2022-02-21 23:21:13 -0500

    delkey: one at a time

--- fish/functions/delkey.fish
+++ fish/functions/delkey.fish
@@ -5,7 +5,7 @@ function delkey
     end
 
     for target in $argv
-        set -l delip (grep "$target " ~/.ssh/known_hosts | awk '{print $1}' | cut -f 2 -d \, | uniq)
+        set -l delip (grep "$target " ~/.ssh/known_hosts | awk '{print $1}' | cut -f 2 -d \, | uniq | tail -n 1)
         # breakpoint
         [ -z $delip ] || ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$delip"
         ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$target"
