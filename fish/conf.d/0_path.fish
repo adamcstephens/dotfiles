a3f42b0424b79aadab8c36d8e1dcda23032d7e1f
Adam Stephens <adam@valkor.net>
2022-03-13 22:27:38 -0400

    reorder fish path

--- fish/conf.d/0_path.fish
+++ fish/conf.d/0_path.fish
@@ -1,14 +1,14 @@
 # paths
-fish_add_path --append ~/bin
-fish_add_path --append ~/.dotfiles/bin
-fish_add_path --append ~/.local/bin
-
 if test -d ~/.emacs.d/bin
-    fish_add_path ~/.emacs.d/bin
+    fish_add_path --prepend --move ~/.emacs.d/bin
 end
 if test -d ~/go/bin
-    fish_add_path ~/go/bin
+    fish_add_path --prepend --move ~/go/bin
 end
 if test -d /snap/bin
-    fish_add_path /snap/bin
+    fish_add_path --prepend --move /snap/bin
 end
+
+fish_add_path --prepend --move ~/.local/bin
+fish_add_path --prepend --move ~/bin
+fish_add_path --prepend --move ~/.dotfiles/bin
