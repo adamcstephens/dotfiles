94988b26b8310f61ea585690d264385d56f8c31d
Adam Stephens <adam@valkor.net>
2022-03-31 21:57:32 -0400

    linux updates, vscode script

--- bin/vscode-extensions.fish
+++ bin/vscode-extensions.fish
@@ -3,30 +3,34 @@
 set extfile ~/.dotfiles/vscode/extensions.txt
 set uninstfile ~/.dotfiles/vscode/extensions-uninstall.txt
 
-if [ "$argv[1]" = install ]
-    sed -i "/$argv[2]/d" $uninstfile
-    code --install-extension $argv[2]
-else if [ "$argv[1]" = remove ]
-    sed -i "/$argv[2]/d" $extfile
-    code --uninstall-extension $argv[2]
-    echo $argv[2] >>$uninstfile
-    exit $status
-end
+for code in code code-insiders
+    if command -q $code
+        if [ "$argv[1]" = install ]
+            sed -i "/$argv[2]/d" $uninstfile
+            $code --install-extension $argv[2]
+        else if [ "$argv[1]" = remove ]
+            sed -i "/$argv[2]/d" $extfile
+            $code --uninstall-extension $argv[2]
+            echo $argv[2] >>$uninstfile
+            exit $status
+        end
 
-set installed (code --list-extensions)
-set repo (cat $extfile)
-set uninstall (cat $uninstfile)
+        set installed ($code --list-extensions)
+        set repo (cat $extfile)
+        set uninstall (cat $uninstfile)
 
-for u in $uninstall
-    if contains $u $installed
-        code --uninstall-extension $u
+        for u in $uninstall
+            if contains $u $installed
+                $code --uninstall-extension $u
+            end
+        end
+
+        for e in $repo
+            if not contains $e $installed
+                $code --install-extension $e
+            end
+        end
+
+        $code --list-extensions >$extfile
     end
 end
-
-for e in $repo
-    if not contains $e $installed
-        code --install-extension $e
-    end
-end
-
-code --list-extensions >$extfile
--- river.sh
+++ river.sh
@@ -157,9 +157,9 @@ riverctl input pointer-1452:613:Apple_Inc._Magic_Trackpad natural-scroll enabled
 riverctl input pointer-1452:613:Apple_Inc._Magic_Trackpad tap enabled
 riverctl input pointer-1452:613:Apple_Inc._Magic_Trackpad tap-button-map left-right-middle
 # riverctl input 1739:52619:SYNA8004:00_06CB:CD8B_Touchpad events disabled
-riverctl input pointer-1739:52619:SYNA8004:00_06CB:CD8B_Touchpad natural-scroll enabled
-riverctl input pointer-1739:52619:SYNA8004:00_06CB:CD8B_Touchpad tap enabled
-riverctl input pointer-1739:52619:SYNA8004:00_06CB:CD8B_Touchpad tap-button-map left-right-middle
+riverctl input pointer-1739-52619-SYNA8004:00_06CB:CD8B_Touchpad natural-scroll enabled
+riverctl input pointer-1739-52619-SYNA8004:00_06CB:CD8B_Touchpad tap enabled
+riverctl input pointer-1739-52619-SYNA8004:00_06CB:CD8B_Touchpad tap-button-map left-right-middle
 riverctl focus-follows-cursor normal
 
 # systemd
