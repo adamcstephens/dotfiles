09495c3ac318ca0ef9d91f40daa86ff8936a8e2c
Adam Stephens <adam@valkor.net>
2022-04-06 14:30:23 -0400

    firefox overrides needs semi colons...

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,8 +1,8 @@
-user_pref("browser.download.useDownloadDir", true)
-user_pref("browser.warnOnQuit", false)
-user_pref("extensions.pocket.enabled", false)
-user_pref("keyword.enabled", true)
-user_pref("network.trr.mode", 5)
-user_pref("privacy.clearOnShutdown.history", false)
-user_pref("privacy.resistFingerprinting.letterboxing", false)
-user_pref("signon.rememberSignons", false)
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.warnOnQuit", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("keyword.enabled", true);
+user_pref("network.trr.mode", 5);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("signon.rememberSignons", false);
