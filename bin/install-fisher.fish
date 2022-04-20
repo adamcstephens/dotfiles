08fe3091ef86203f5322958bd23344334b102f31
Adam Stephens <adam@valkor.net>
2022-04-20 15:51:11 -0400

    finish taskfile migration and cleanup

--- bin/install-fisher.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-source ~/.dotfiles/fish/functions/fisher.fish
-fisher install jorgebucaran/fisher
-
-fisher install decors/fish-colored-man
-fisher install evanlucas/fish-kubectl-completions
-fisher install franciscolourenco/done
-fisher install PatrickF1/fzf.fish
