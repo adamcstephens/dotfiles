92aa24cf28ba079d990de57deda4da61d77c6e1f
Adam Stephens <adam@valkor.net>
2022-05-08 23:59:06 -0400

    add ansible-doc searching

--- fish/functions/ad.fish
+++ fish/functions/ad.fish
@@ -1,3 +1,7 @@
 function ad --wraps='ansible-doc ' --wraps=ansible-doc --description 'alias ad=ansible-doc'
-    ansible-doc $argv
+    if [ -z $argv[1] ]
+        ansible-doc (ansible-doc -l 2>/dev/null | fzf --no-hscroll | awk '{print $1}')
+    else
+        ansible-doc $argv
+    end
 end
