ff4578778dda6dc8232908209b0197ec1ca031a1
Adam Stephens <adam@valkor.net>
2022-06-01 22:01:02 -0400

    work around fisher writing to vars file

--- bin/install-fisher.fish
+++ bin/install-fisher.fish
@@ -1,7 +1,11 @@
 #!/usr/bin/env fish
 
+set pkgs (cat ~/.dotfiles/fish/fish_plugins)
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
 source ~/.dotfiles/fish/functions/fisher.fish
 
-for pkg in (cat ~/.dotfiles/fish/fish_plugins)
+for pkg in $pkgs
     fisher install $pkg
 end
