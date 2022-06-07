cda3bd259e1a6c65f1e59634f31e77a75289bf9c
Adam Stephens <adam@valkor.net>
2022-06-06 23:27:49 -0400

    initial rewrite of vscode script

---
+++ bin/vscode-extensions.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+
+set -x
+
+EXTENSIONS_FILE="$HOME/.dotfiles/vscode/extensions.txt"
+UNINSTALL_FILE="$HOME/.dotfiles/vscode/extensions-uninstall.txt"
+
+for code in code code-insiders; do
+  if command -v $code >/dev/null 2>&1; then
+    case "$(which code)" in
+    *bin/remote-cli/*)
+      exit 0
+      ;;
+    *)
+      case $1 in
+      install)
+        sed -i "/$2/d" "$UNINSTALL_FILE"
+        $code --install-extension "$2"
+        ;;
+      remove)
+        sed -i "/$2/d" "$EXTENSIONS_FILE"
+        $code --uninstall-extension "$2"
+        echo "$2" >>"$UNINSTALL_FILE"
+        exit $?
+        ;;
+      esac
+
+      installed="$($code --list-extensions)"
+      repo="$(cat "$EXTENSIONS_FILE")"
+      uninstall="$(cat "$UNINSTALL_FILE")"
+
+      for u in $uninstall; do
+        if echo "$installed" | grep "$u" >/dev/null; then
+          $code --uninstall-extension "$u"
+        fi
+      done
+
+      for e in $repo; do
+        if ! (echo "$installed" | grep "e"); then
+          $code --install-extension "$e"
+        fi
+      done
+
+      $code --list-extensions >"$EXTENSIONS_FILE"
+      ;;
+    esac
+  fi
+done
