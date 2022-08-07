1318cb097450fe6acdf645305b672bf3bd271c74
Adam Stephens <adam@valkor.net>
2022-08-07 00:06:03 -0400

    windows checkin

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -3,3 +3,9 @@ $ENV:STARSHIP_CONFIG = "$HOME\.dotfiles\starship.toml"
 
 $SSHPath = (Get-Command -Name 'ssh.exe').Source
 $ENV:GIT_SSH = $SSHPath
+
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+Set-PSReadLineOption -PredictionSource history
+# Set-PSReadLineOption -Colors @{ InlinePrediction = '#000055'}
+Set-PSReadLineKeyHandler -chord Ctrl+e -function ForwardChar
