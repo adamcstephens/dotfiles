d6578463f35463c325f665b95703cb8981fa5668
Adam Stephens <adam@valkor.net>
2022-09-06 01:55:38 +0000

    git ssh key sign using agent

--- bin/git-ssh-key.sh
+++ bin/git-ssh-key.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 
-if [ ! -e ~/.ssh/id_ed25519 ]; then
-  echo "Missing key ~/.ssh/id_ed25519"
+if [ -e ~/.ssh/id_ed25519 ]; then
+  key="$(cat ~/.ssh/id_ed25519.pub)"
+elif [ -n "$SSH_AUTH_SOCK" ]; then
+  key="$(ssh-add -L | head -n 1)"
+fi
+
+if [ -z "$key" ]; then
+  echo "Failed to detect key. ibailout"
   exit 1
 fi
 
-echo "key::$(cat ~/.ssh/id_ed25519.pub)"
+echo "key::$key"
