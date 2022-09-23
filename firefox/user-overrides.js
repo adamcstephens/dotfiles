4f51b373bde3553268f3e9ddf0a6a051eab54649
Adam Stephens <adam@valkor.net>
2022-09-23 09:21:36 -0400

    firefox: force disable form filling

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -2,6 +2,9 @@ user_pref("browser.download.useDownloadDir", true);
 user_pref("browser.warnOnQuit", false);
 // * 0=blank, 1=home, 2=last visited page, 3=resume previous session
 user_pref("browser.startup.page", 3);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("geo.enabled", false);
 user_pref("keyword.enabled", true);
