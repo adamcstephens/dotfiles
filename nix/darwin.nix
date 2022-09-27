8c2c94f4aa8b071da4191bcf8a7baba13252d509
Adam Stephens <adam@valkor.net>
2022-09-27 10:53:08 -0400

    move dotbot multi-config to nix, rename files

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -1,5 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
   programs.doom-emacs = {
     emacsPackage = pkgs.emacsNativeComp;
   };
+
+  home.activation.dotfiles-bootstrap-darwin = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    export PATH=${config.home.path}/bin:${config.home.path}/sbin:$PATH
+
+    pushd ~/.dotfiles
+      CONFIG=dotbot.Darwin.yaml task dotbot
+    popd
+  '';
 }
--- nix/linux-gui.nix
+++ nix/linux-gui.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = [
     # pkgs.cider
   ];
@@ -6,4 +11,12 @@
   programs.doom-emacs = {
     emacsPackage = pkgs.emacsPgtkNativeComp;
   };
+
+  home.activation.dotfiles-bootstrap-linux-gui = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    export PATH=${config.home.path}/bin:${config.home.path}/sbin:$PATH
+
+    pushd ~/.dotfiles
+      CONFIG=dotbot.linux-gui.yaml task dotbot
+    popd
+  '';
 }
