44fc64487e476d627ccedb015aca523d8ec95d25
Adam Stephens <adam@valkor.net>
2022-09-27 08:31:38 -0400

    refactor hm switcher

--- nix/hm.nix
+++ nix/hm.nix
@@ -19,42 +19,43 @@ in rec {
     else:
         print("${pkgs.stdenv.hostPlatform.system}")
   '';
+  hm = pkgs.writeScriptBin "hm" ''
+    set -e
 
-  hm-build = pkgs.writeScriptBin "hm-build" ''
     HMPROFILE="$(${home-profile-selector}/bin/home-profile-selector)"
+
+    if [ -n "$1" ]; then
+     ACTION=$1
+    fi
+
+    : "''${ACTION:=build}"
+
+    echo " Running action $ACTION"
 
     echo "🚧 Building new profile for $HMPROFILE"
     ${nixCmd} build --no-link .#homeConfigurations.$HMPROFILE.activationPackage || exit 1
-  '';
 
-  hm-push = pkgs.writeScriptBin "hm-push" ''
-    if [ -z "$1" ]; then
-      HMPROFILE="$(${home-profile-selector}/bin/home-profile-selector)"
-    else
-      HMPROFILE="$1"
+    if [ "$ACTION" = "switch" ]; then
+      old_profile=$(${nixCmd} profile list | grep home-manager-path | head -n1 | awk '{print $4}')
+      if [ -n "$old_profile" ]; then
+        echo "❌ Removing old profile: $old_profile"
+        ${nixCmd} profile remove $old_profile
+      fi
+
+      echo " Activating new profile"
+      if ! "$(${nixCmd} path-info .#homeConfigurations.$HMPROFILE.activationPackage)"/activate; then
+        echo "❗ Failed to activate new profile"
+        echo " Rolling back to old profile"
+        ${nixCmd} profile install $old_profile
+        exit 1
+      fi
     fi
 
-    echo "cachix upload for $HMPROFILE"
-    ${nixCmd} build --no-link .#homeConfigurations.$HMPROFILE.activationPackage --json | jq -r '.[].outputs | to_entries[].value' | ${pkgs.cachix}/bin/cachix push ${cachixRepo}
-  '';
-
-  hm-switch = pkgs.writeScriptBin "hm-update" ''
-    HMPROFILE="$(${home-profile-selector}/bin/home-profile-selector)"
-
-    ${hm-build}/bin/hm-build
-
-    old_profile=$(${nixCmd} profile list | grep home-manager-path | head -n1 | awk '{print $4}')
-    if [ -n "$old_profile" ]; then
-      echo "❌ Removing old profile: $old_profile"
-      ${nixCmd} profile remove $old_profile
+    if [ "$ACTION" = "push" ]; then
+      echo " cachix upload for $HMPROFILE"
+      ${nixCmd} build --no-link .#homeConfigurations.$HMPROFILE.activationPackage --json | jq -r '.[].outputs | to_entries[].value' | ${pkgs.cachix}/bin/cachix push ${cachixRepo}
     fi
 
-    echo " Activating new profile"
-    if ! "$(${nixCmd} path-info .#homeConfigurations.$HMPROFILE.activationPackage)"/activate; then
-      echo "❗ Failed to activate new profile"
-      echo " Rolling back to old profile"
-      ${nixCmd} profile install $old_profile
-      exit 1
-    fi
+    echo "✅ Success!"
   '';
 }
