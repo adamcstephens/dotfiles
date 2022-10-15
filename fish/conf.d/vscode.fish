20333e8884cfece0032107c70a5b24e5720aaf41
Adam Stephens <adam@valkor.net>
2022-10-15 14:21:01 +0000

    better fish vscode remote handling, and bug workaround

--- fish/conf.d/vscode.fish
+++ fish/conf.d/vscode.fish
@@ -1,2 +1,12 @@
-string match -q "$TERM_PROGRAM" vscode
-and . (code --locate-shell-integration-path fish 2>/dev/null)
+if status is-interactive && string match -q "$TERM_PROGRAM" vscode
+    if ! command -q code
+        set VSCODE_PATH (echo $BROWSER | sed 's,helpers/browser.sh,remote-cli,')
+        if test -d $VSCODE_PATH
+            fish_add_path $VSCODE_PATH
+        end
+    end
+
+    if command -q code
+        . (code --locate-shell-integration-path fish 2>/dev/null | sed 's/shellIntegration-fish/shellIntegration/')
+    end
+end
