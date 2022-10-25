27919dd19cab9960fc95d2a05e010ded60f553e5
Adam Stephens <adam@valkor.net>
2022-10-25 11:36:42 -0400

    and more moves to apps/

--- apps/fish/functions/fmigalias.fish
+++ apps/fish/functions/fmigalias.fish
@@ -2,5 +2,5 @@ function fmigalias
     set -l myAlias $argv[1]
     rg -N "alias $myAlias=" ~/.shell_generic.sh | sed -e 's,\\\\\$,,g' | source -
     funcsave $myAlias
-    fish_indent --write ~/.dotfiles/fish/functions/$myAlias.fish
+    fish_indent --write ~/.dotfiles/apps/fish/functions/$myAlias.fish
 end
