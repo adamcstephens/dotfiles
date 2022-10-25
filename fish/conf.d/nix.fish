7a72a930db008439ca81ba2bb67924b09d65f2d3
Adam Stephens <adam@valkor.net>
2022-10-25 10:30:28 -0400

    fish: read completions and functions from HM

--- fish/conf.d/nix.fish
+++ fish/conf.d/nix.fish
@@ -3,5 +3,13 @@ if status is-interactive
         fenv source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
     end
 
+    if test -e ~/.nix-profile/share/fish/vendor_completions.d
+        set --prepend --export fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d
+    end
+
+    if test -e ~/.nix-profile/share/fish/vendor_functions.d
+        set --prepend --export fish_function_path ~/.nix-profile/share/fish/vendor_functions.d
+    end
+
     set -x XDG_DATA_DIRS "$HOME/.nix-profile/share/:$XDG_DATA_DIRS"
 end
