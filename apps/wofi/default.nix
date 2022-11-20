04155de57510f79318183bef005d0024aeb4d0ec
Adam Stephens <adam@valkor.net>
2022-11-20 12:34:07 -0500

    wofi to use nix-colors

--- apps/wofi/default.nix
+++ apps/wofi/default.nix
@@ -1,6 +1,47 @@
-{pkgs, ...}: {
-  xdg.configFile."wofi/config".source = ./config;
-  xdg.configFile."wofi/style.css".source = ./style.css;
+{
+  config,
+  pkgs,
+  ...
+}: {
+  xdg.configFile."wofi/config".text = ''
+    width=800
+    height=400
+    insensitive=true
+    mode=drun,run
+  '';
+
+  xdg.configFile."wofi/style.css".text = ''
+    window {
+      border: 2px solid #${config.colorScheme.colors.base03};
+      background-color: #${config.colorScheme.colors.base00};
+    }
+
+    #input {
+      color: #${config.colorScheme.colors.base09};
+      border: 2px solid #${config.colorScheme.colors.base03};
+      background-color: #${config.colorScheme.colors.base00};
+      font-size: 18px;
+    }
+
+    #outer-box {
+      margin: 10px;
+    }
+
+    #scroll {
+      margin: 5px 0px;
+      font-size: 16px;
+      color: #${config.colorScheme.colors.base0B};
+    }
+
+    #scroll label {
+      margin: 2px 0px;
+    }
+
+    #entry:selected {
+      color: #${config.colorScheme.colors.base0B};
+      background-color: #${config.colorScheme.colors.base00};
+    }
+  '';
 
   home.packages = [
     pkgs.wofi
