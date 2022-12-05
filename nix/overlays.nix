fcfb8759c51dd49aba7e26a0a7181f922f9de580
Adam Stephens <adam@valkor.net>
2022-12-04 23:09:30 -0500

    use pkgs.gtklock

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -6,12 +6,10 @@
   default = nixpkgs.lib.composeManyExtensions [
     emacs.overlays.emacs
     emacs.overlays.package
-    gtklock
     fishPlugins
     terminfo
   ];
 
-  gtklock = _: prev: {gtklock = prev.callPackage ./packages/gtklock.nix {};};
   terminfo = _: prev: {terminfo = prev.callPackage ./packages/terminfo {};};
 
   # disable tests since they broke on darwin...
