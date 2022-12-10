b82ee2196d0e016a40f1cebae297a301739ee9b6
Adam Stephens <adam@valkor.net>
2022-12-10 14:47:51 -0500

    swayidle to 1.8.0

--- apps/swayidle/default.nix
+++ apps/swayidle/default.nix
@@ -4,25 +4,36 @@
   lib,
   ...
 }: let
-  gtklockBin = "${pkgs.gtklock}/bin/gtklock";
   systemctlBin = "/run/current-system/sw/bin/systemctl";
+  locker = "${pkgs.procps}/bin/pgrep gtklock || ${pkgs.util-linux}/bin/setsid --fork ${pkgs.gtklock}/bin/gtklock";
+  swayidle = pkgs.swayidle.overrideAttrs (old: rec {
+    version = "1.8.0";
 
-  gtklock = "${pkgs.procps}/bin/pgrep gtklock || ${pkgs.util-linux}/bin/setsid --fork ${gtklockBin}";
+    postPatch = null;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "swaywm";
+      repo = "swayidle";
+      rev = version;
+      sha256 = "sha256-/U6Y9H5ZqIJph3TZVcwr9+Qfd6NZNYComXuC1D9uGHg=";
+    };
+  });
 in {
   services.swayidle = {
     enable = true;
+    package = swayidle;
     systemdTarget = "graphical-session.target";
     events = [
       {
         event = "before-sleep";
-        command = "${gtklock}";
+        command = "${locker}";
       }
     ];
     timeouts =
       [
         {
           timeout = 120;
-          command = "${gtklock}";
+          command = "${locker}";
         }
       ]
       ++ (lib.optionals (!config.dotfiles.isVM) [
