b75e15d74cae6cefbd74140a8320b80e55693e7d
Adam Stephens <adam@valkor.net>
2022-12-19 09:39:17 -0500

    hey i see a terminal!

--- apps/xmonad/default.nix
+++ apps/xmonad/default.nix
@@ -1,6 +1,20 @@
-{...}: {
-  xsession.windowManager.xmonad = {
+{pkgs, ...}: {
+  home.file.".xinitrc".source = ./xinitrc;
+
+  xsession = {
     enable = true;
-    enableContribAndExtras = true;
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+
+      config = pkgs.writeText "xmonad.hs" ''
+        import XMonad
+        main = xmonad defaultConfig
+            { terminal    = "kitty"
+            , modMask     = mod4Mask
+            , borderWidth = 3
+            }
+      '';
+    };
   };
 }
