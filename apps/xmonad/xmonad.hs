2fc919430662990b64b8399ffb8128a0f029042b
Adam Stephens <adam@valkor.net>
2022-12-19 22:29:45 -0500

    theme xmonad and add another terminal shortcut

--- apps/xmonad/xmonad.hs
+++ apps/xmonad/xmonad.hs
@@ -5,7 +5,11 @@ import XMonad.Config.Desktop
 main = xmonad $ desktopConfig
   { terminal    = "kitty"
   , modMask     = mod4Mask
-  , borderWidth = 3
+  -- , borderWidth = 3
+  , normalBorderColor = "#3E4B59"
+  , focusedBorderColor = "#E6E1CF"
   }
   `additionalKeysP`
-  [ ("M-d", spawn "systemd-cat --identifier=rofi rofi -show drun -dpi 192") ]
+  [ ("M-d", spawn "systemd-cat --identifier=rofi rofi -show drun -dpi 192")
+  , ("M-S-t", spawn "systemd-cat --identifier=kitty kitty --single-instance")
+  ]
