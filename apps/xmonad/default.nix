e3faf12a01219851f496552fb258b83e6927af6f
Adam Stephens <adam@valkor.net>
2022-12-19 22:29:18 -0500

    add locker

--- apps/xmonad/default.nix
+++ apps/xmonad/default.nix
@@ -14,4 +14,11 @@
       config = ./xmonad.hs;
     };
   };
+
+  services.screen-locker = {
+    enable = true;
+    xautolock.detectSleep = true;
+    inactiveInterval = 15;
+    lockCmd = "${pkgs.xsecurelock}/bin/xsecurelock";
+  };
 }
