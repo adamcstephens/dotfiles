8f578fa8305e1b6b805d35db1fa8683cb1d14add
Adam Stephens <adam@valkor.net>
2022-12-20 13:39:15 -0500

    customize xsecurelock

--- apps/xmonad/default.nix
+++ apps/xmonad/default.nix
@@ -1,8 +1,20 @@
 {
+  config,
   lib,
   pkgs,
   ...
-}: {
+}: let
+  colors = config.colorScheme.colors;
+  xsecurelock = pkgs.writeScript "xsecurelock" ''
+    export XSECURELOCK_COMPOSITE_OBSCURER=0
+    export XSECURELOCK_BACKGROUND_COLOR="#${colors.base00}"
+    export XSECURELOCK_AUTH_BACKGROUND_COLOR="#${colors.base00}"
+    export XSECURELOCK_AUTH_FOREGROUND_COLOR="#${colors.base05}"
+    export XSECURELOCK_FONT="JetBrainsMono Nerd Font"
+
+    ${pkgs.xsecurelock}/bin/xsecurelock
+  '';
+in {
   home.file.".xinitrc".source = ./xinitrc;
 
   xresources.properties = {
@@ -23,7 +35,7 @@
     enable = true;
     xautolock.detectSleep = true;
     inactiveInterval = 15;
-    lockCmd = "${pkgs.xsecurelock}/bin/xsecurelock";
+    lockCmd = xsecurelock.outPath;
   };
 
   systemd.user.services.xautolock-session.Install.WantedBy = lib.mkForce ["xserver-session.target"];
