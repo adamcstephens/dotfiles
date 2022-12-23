9d100031eb1c130426fa1fa06092d3c0367ba860
Adam Stephens <adam@valkor.net>
2022-12-23 09:58:34 -0500

    fix fzf C-t searching

--- apps/fish/conf.d/fzf-config.fish
+++ apps/fish/conf.d/fzf-config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     fzf_configure_bindings --directory=\ct
     set fzf_fd_opts --hidden --exclude=.git
+
+    set -x FZF_CTRL_T_COMMAND 'fd --type f --exclude .git --exclude vendor'
 end
