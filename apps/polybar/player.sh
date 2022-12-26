67cb3f79d7a52a6debcfdd7405b4af2da25b7804
Adam Stephens <adam@valkor.net>
2022-12-26 10:38:26 -0500

    tweak polybar

--- apps/polybar/player.sh
+++ apps/polybar/player.sh
@@ -2,7 +2,11 @@
 
 case $(playerctl status 2>/dev/null) in
 Playing)
-  echo "$(playerctl metadata title) | $(playerctl metadata artist)"
+  title="$(playerctl metadata title)"
+  artist="$(playerctl metadata artist)"
+  printf "%s" "$title"
+  [ -n "$artist" ] && printf '\- %s' "$artist"
+  echo
   ;;
 *) echo "" ;;
 esac
