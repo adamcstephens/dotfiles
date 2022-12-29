7d0e7dbf053feb152f648e786b911b8a7e489832
Adam Stephens <adam@valkor.net>
2022-12-29 09:42:26 -0500

    git commit -v support, dynamic gutter

--- apps/emacs/dotemacs/elisp/init-git.el
+++ apps/emacs/dotemacs/elisp/init-git.el
@@ -3,6 +3,12 @@
 
 (use-package git-gutter
   :init
-  (global-git-gutter-mode t))
+  (global-git-gutter-mode t)
+  (setq git-gutter:hide-gutter t)
+  (add-to-list 'git-gutter:update-hooks 'focus-in-hook)
+  (add-to-list 'git-gutter:update-commands 'other-window))
+
+;; load diff mode on cli commit
+(add-to-list 'auto-mode-alist '("/COMMIT_EDITMSG\\'" . diff-mode))
 
 (provide 'init-git)
