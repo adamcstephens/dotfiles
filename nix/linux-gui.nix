0fd6a11fa6b869fc74bdf4493c3b94fb743dbcd4
Adam Stephens <adam@valkor.net>
2022-12-29 11:31:44 -0500

    move xdg default apps to dotfiles

--- nix/linux-gui.nix
+++ nix/linux-gui.nix
@@ -124,4 +124,17 @@ in {
       After = ["graphical-session-pre.target"];
     };
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "default-web-browser" = ["firefox.desktop"];
+      "text/html" = ["firefox.desktop"];
+      "application/pdf" = ["firefox.desktop"];
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+      "x-scheme-handler/about" = ["firefox.desktop"];
+      "x-scheme-handler/unknown" = ["firefox.desktop"];
+    };
+  };
 }
