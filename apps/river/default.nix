f51b3317d87f6c4de298cf5a766f7ab1880e3125
Adam Stephens <adam@valkor.net>
2023-01-04 19:01:50 -0500

    river use nixpkgs package

--- apps/river/default.nix
+++ apps/river/default.nix
@@ -4,33 +4,23 @@
   pkgs,
   ...
 }: let
-  # latest
-  # rev = "e603c5460a27bdc8ce6c32c8ee5e53fb789bc10b";
-  # hash = "sha256-x971VRWp72uNRNcBTU2H81EiqWa5kg0E5n7tK8ypaQM=";
-  # known-good
-  # rev = "e35c147cd5b8fcd363b7ecc495292733b25d96f5";
-  # hash = "sha256-orKL3imxpQXrSLj12Z3Zn5UuAW7P/JeOfoWCkb98eCM=";
   rev = "refs/tags/v0.2.0";
   hash = "sha256-BrOZch6wkiBB4rk0M7Aoy8sZh8uOTQFOPxd3xLyy/K0=";
 
   devVer = builtins.substring 0 8 rev;
-  river =
-    (pkgs.river.override {
-      wlroots = pkgs.wlroots_0_16;
-    })
-    .overrideAttrs (_: {
-      version = "0.2.0";
-      src = pkgs.fetchFromGitHub {
-        inherit rev hash;
+  river = pkgs.river.overrideAttrs (_: {
+    version = "0.2.0";
+    src = pkgs.fetchFromGitHub {
+      inherit rev hash;
 
-        owner = "riverwm";
-        repo = "river";
-        fetchSubmodules = true;
-      };
-    });
+      owner = "riverwm";
+      repo = "river";
+      fetchSubmodules = true;
+    };
+  });
 in {
   home.packages = [
-    river
+    pkgs.river
   ];
 
   xdg.configFile."river/init" = {
