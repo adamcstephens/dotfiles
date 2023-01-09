06b9021d3a454169ac77644cc679a64914e6f5c7
Adam Stephens <adam@valkor.net>
2023-01-08 21:35:26 -0500

    cleanup

--- apps/emacs/dotemacs/elisp/init-project.el
+++ apps/emacs/dotemacs/elisp/init-project.el
@@ -13,16 +13,6 @@
   (direnv-mode)
   (setq direnv-always-show-summary nil))
 
-(use-package project-tab-groups
-  :disabled
-  :config
-  (project-tab-groups-mode 1))
-
-(use-package tab-bar-echo-area
-  :disabled
-  :config
-  (tab-bar-echo-area-mode 1))
-
 (use-package consult-project-extra
   :straight t
   :init
@@ -31,23 +21,13 @@
   (("C-c p f" . consult-project-extra-find)
    ("H-SPC" . consult-project-extra-find)))
 
-;; load dired instead of prompting
 ;; project keys
 (global-set-key (kbd "C-c p p") 'project-switch-project)
+;; load dired instead of prompting
 (global-set-key (kbd "C-c p d") 'project-dired)
 
 ;; enable tab mode but hide the bar
 (setq tab-bar-show nil)
 (tab-bar-mode 1)
-
-;; tab keys
-;; (global-set-key (kbd "C-c t s") 'tab-switch)
-;; (global-set-key (kbd "C-c t j") 'tab-previous)
-;; (global-set-key (kbd "C-c t k") 'tab-next)
-;; (global-set-key (kbd "C-c t l") 'tab-bar-switch-to-recent-tab)
-
-;; add a default tab layout
-;; (tab-new)
-;; (tab-rename "Main" 1)
 
 (provide 'init-project)
