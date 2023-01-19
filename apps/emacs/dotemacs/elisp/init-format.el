225a61960c667eb342f2cb50c4cc69d4a426ada3
Adam Stephens <adam@valkor.net>
2023-01-18 23:11:21 -0500

    add elisp-autofmt

--- apps/emacs/dotemacs/elisp/init-format.el
+++ apps/emacs/dotemacs/elisp/init-format.el
@@ -1,9 +1,17 @@
-(use-package format-all
-  :diminish
+(use-package
+  format-all
   :config
   (progn
     (add-hook 'prog-mode-hook 'format-all-mode)
     (add-hook 'prog-mode-hook 'format-all-ensure-formatter)))
+
+(use-package
+  elisp-autofmt
+  :hook (emacs-list-mode . elisp-autofmt-mode)
+  :custom
+  (elisp-autofmt-empty-line-max 1)
+  (elisp-autofmt-on-save-p 'always)
+  (elisp-autofmt-style 'fixed))
 
 ;; (use-package apheleia
 ;;   :config
