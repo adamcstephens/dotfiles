e0fe68128bf7da808c822e50a45c9bdebe0423fc
Adam Stephens <adam@valkor.net>
2023-01-20 15:30:41 -0500

    use avy-goto-line

--- apps/emacs/dotemacs/elisp/init-modal.el
+++ apps/emacs/dotemacs/elisp/init-modal.el
@@ -1,10 +1,10 @@
-(use-package evil
+(use-package
+  evil
   :disabled
   :init
   (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
   (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1)
+  :config (evil-mode 1)
 
   ;; set up the undo system
   (setq evil-undo-system 'undo-fu)
@@ -13,144 +13,169 @@
   (evil-set-leader nil (kbd "C-SPC"))
   ;; set leader key in normal state
   (evil-set-leader 'normal (kbd "SPC"))
-  (evil-define-key 'normal 'global (kbd "<leader>b") 'workroom-switch-to-buffer)
+  (evil-define-key
+    'normal
+    'global
+    (kbd "<leader>b")
+    'workroom-switch-to-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>d") 'dired-at-point)
   (evil-define-key 'normal 'global (kbd "<leader>fs") 'save-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>gg") 'magit)
   (evil-define-key 'normal 'global (kbd "<leader>fs") 'save-buffer)
-  (evil-define-key 'normal 'global (kbd "<leader>pp") 'project-switch-project)
-  (evil-define-key 'normal 'global (kbd "<leader>ps") 'consult-ripgrep)
-  (evil-define-key 'normal 'global (kbd "<leader>pw") 'workroom-switch)
+  (evil-define-key
+    'normal
+    'global
+    (kbd "<leader>pp")
+    'project-switch-project)
+  (evil-define-key
+    'normal
+    'global
+    (kbd "<leader>ps")
+    'consult-ripgrep)
+  (evil-define-key
+    'normal
+    'global
+    (kbd "<leader>pw")
+    'workroom-switch)
   (evil-define-key 'normal 'global (kbd "<leader>v") 'vterm)
 
   ;; unbind return in evil so org links can be followed
-  (with-eval-after-load 'evil-maps (define-key evil-motion-state-map (kbd "RET") nil))
-  )
+  (with-eval-after-load 'evil-maps
+    (define-key evil-motion-state-map (kbd "RET") nil)))
 
-(use-package evil-collection
+(use-package
+  evil-collection
   :disabled
   :after evil
-  :config
-  (evil-collection-init))
+  :config (evil-collection-init))
 
-(use-package evil-mc
+(use-package
+  evil-mc
   :disabled
   :after evil
-  :config
-  (global-evil-mc-mode 1)
+  :config (global-evil-mc-mode 1)
 
   ;; override visual mode to mc every line
-  (evil-define-key 'visual evil-mc-key-map "A" #'evil-mc-make-cursor-in-visual-selection-end)
-  (evil-define-key 'visual evil-mc-key-map "I" #'evil-mc-make-cursor-in-visual-selection-beg))
+  (evil-define-key
+    'visual
+    evil-mc-key-map
+    "A"
+    #'evil-mc-make-cursor-in-visual-selection-end)
+  (evil-define-key
+    'visual
+    evil-mc-key-map
+    "I"
+    #'evil-mc-make-cursor-in-visual-selection-beg))
 
 (defun dot/meow-setup ()
+  (interactive)
   (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
   (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
+    '("j" . meow-next)
+    '("k" . meow-prev)
+    '("<escape>" . ignore))
   (meow-leader-define-key
-   ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j")
-   '("k" . "H-k")
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
+    ;; SPC j/k will run the original command in MOTION state.
+    '("j" . "H-j")
+    '("k" . "H-k")
+    ;; Use SPC (0-9) for digit arguments.
+    '("1" . meow-digit-argument)
+    '("2" . meow-digit-argument)
+    '("3" . meow-digit-argument)
+    '("4" . meow-digit-argument)
+    '("5" . meow-digit-argument)
+    '("6" . meow-digit-argument)
+    '("7" . meow-digit-argument)
+    '("8" . meow-digit-argument)
+    '("9" . meow-digit-argument)
+    '("0" . meow-digit-argument)
+    '("/" . meow-keypad-describe-key)
+    '("?" . meow-cheatsheet))
   (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . undo-fu-only-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("/" . isearch-forward-regexp)
-   '("<" . indent-rigidly-left-to-tab-stop)
-   '(">" . indent-rigidly-right-to-tab-stop)
-   '("<escape>" . ignore)))
+    '("0" . meow-expand-0)
+    '("9" . meow-expand-9)
+    '("8" . meow-expand-8)
+    '("7" . meow-expand-7)
+    '("6" . meow-expand-6)
+    '("5" . meow-expand-5)
+    '("4" . meow-expand-4)
+    '("3" . meow-expand-3)
+    '("2" . meow-expand-2)
+    '("1" . meow-expand-1)
+    '("-" . negative-argument)
+    '(";" . meow-reverse)
+    '("," . meow-inner-of-thing)
+    '("." . meow-bounds-of-thing)
+    '("[" . meow-beginning-of-thing)
+    '("]" . meow-end-of-thing)
+    '("a" . meow-append)
+    '("A" . meow-open-below)
+    '("b" . meow-back-word)
+    '("B" . meow-back-symbol)
+    '("c" . meow-change)
+    '("d" . meow-delete)
+    '("D" . meow-backward-delete)
+    '("e" . meow-next-word)
+    '("E" . meow-next-symbol)
+    '("f" . meow-find)
+    '("g" . meow-cancel-selection)
+    '("G" . meow-grab)
+    '("h" . meow-left)
+    '("H" . meow-left-expand)
+    '("i" . meow-insert)
+    '("I" . meow-open-above)
+    '("j" . meow-next)
+    '("J" . meow-next-expand)
+    '("k" . meow-prev)
+    '("K" . meow-prev-expand)
+    '("l" . meow-right)
+    '("L" . meow-right-expand)
+    '("m" . meow-join)
+    '("n" . meow-search)
+    '("o" . meow-block)
+    '("O" . meow-to-block)
+    '("p" . meow-yank)
+    '("q" . meow-quit)
+    '("Q" . meow-goto-line)
+    '("r" . meow-replace)
+    '("R" . meow-swap-grab)
+    '("s" . meow-kill)
+    '("t" . meow-till)
+    '("u" . undo-fu-only-undo)
+    '("U" . meow-undo-in-selection)
+    '("v" . meow-visit)
+    '("w" . meow-mark-word)
+    '("W" . meow-mark-symbol)
+    '("x" . meow-line)
+    '("X" . avy-goto-line)
+    '("y" . meow-save)
+    '("Y" . meow-sync-grab)
+    '("z" . meow-pop-selection)
+    '("'" . repeat)
+    '("/" . isearch-forward-regexp)
+    '("<" . indent-rigidly-left-to-tab-stop)
+    '(">" . indent-rigidly-right-to-tab-stop)
+    '("<escape>" . ignore)))
 
-(use-package meow
-  :config
-  (dot/meow-setup)
-  (meow-global-mode 1)
+(use-package
+  meow
+  :config (dot/meow-setup) (meow-global-mode 1)
   ;; use system clipboard
   (setq meow-use-clipboard t)
   ;; reduce delay to avoid spurious commands
   (setq meow-esc-delay 0.05))
 
-(use-package xah-fly-keys
+(use-package
+  xah-fly-keys
   :disabled
   :config
   (xah-fly-keys-set-layout "qwerty")
   (xah-fly-keys 1))
 
-(use-package boon
+(use-package
+  boon
   :disabled
-  :config
-  (require 'boon-qwerty)
+  :config (require 'boon-qwerty)
   ;; (boon-mode) ;; to enable boon everywhere
   )
 
