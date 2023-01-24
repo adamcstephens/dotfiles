ed3b463d343ed6fb6265f87762c197e75e0e33bc
Adam Stephens <adam@valkor.net>
2023-01-24 07:52:23 -0500

    add aarch64-linux

--- nix/dev-flakes/nixpkgs/flake.nix
+++ nix/dev-flakes/nixpkgs/flake.nix
@@ -5,7 +5,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-darwin" "aarch64-linux"];
       perSystem = {pkgs, ...}: {
         devShells.default = pkgs.mkShellNoCC {
           packages = [
