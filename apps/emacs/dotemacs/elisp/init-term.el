a0ca8fdd4d8ca677331728143dbfcb1237803380
Adam Stephens <adam@valkor.net>
2023-01-29 09:52:59 -0500

    add windmove up/down in term and format

--- apps/emacs/dotemacs/elisp/init-term.el
+++ apps/emacs/dotemacs/elisp/init-term.el
@@ -1,14 +1,18 @@
-(use-package vterm
+(use-package
+  vterm
   :bind
-  (:map vterm-mode-map
-	("C-u" . (lambda () (interactive) (vterm-send-key (kbd "C-u"))))
-	("M-h" . windmove-left)
-	("M-l" . windmove-right))
-  :custom
-  (vterm-max-scrollback 41000))
+  (:map
+    vterm-mode-map
+    ("C-u" .
+      (lambda ()
+        (interactive)
+        (vterm-send-key (kbd "C-u"))))
+    ("M-h" . windmove-left)
+    ("M-l" . windmove-right)
+    ("M-j" . windmove-up)
+    ("M-k" . windmove-down))
+  :custom (vterm-max-scrollback 41000))
 
-(use-package multi-vterm
-  :bind
-  ("C-c v" . multi-vterm-project))
+(use-package multi-vterm :bind ("C-c v" . multi-vterm-project))
 
 (provide 'init-term)
