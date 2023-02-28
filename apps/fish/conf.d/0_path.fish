eaf8dc16a862f27e828dc99fab5fe41a7527fb32
Adam Stephens <adam@valkor.net>
2023-02-28 11:21:51 -0500

    refactor fish to load nix env correctly

--- apps/fish/conf.d/0_path.fish
+++ apps/fish/conf.d/0_path.fish
@@ -1,26 +1,6 @@
-if status is-interactive && ! command -q nix
-    if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-    else if test -e $HOME/.nix-profile/etc/profile.d/nix.sh
-        fenv source $HOME/.nix-profile/etc/profile.d/nix.sh
-    end
-end
-
 # paths
 fish_add_path --prepend --move ~/bin
 fish_add_path --prepend --move ~/.dotfiles/bin
-
-if test -e ~/.nix-profile/share/fish/vendor_completions.d
-    set --prepend --export fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d
-end
-
-if test -e ~/.nix-profile/share/fish/vendor_functions.d
-    set --prepend --export fish_function_path ~/.nix-profile/share/fish/vendor_functions.d
-end
-
-if test -e ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-    fenv source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-end
 
 set -x XDG_DATA_DIRS "$HOME/.nix-profile/share/:$XDG_DATA_DIRS"
 
--- apps/fish/default.nix
+++ apps/fish/default.nix
@@ -16,6 +16,28 @@ in {
     enable = true;
     plugins = [];
 
+    shellInit = ''
+      if test -e $HOME/.nix-profile/share/fish/vendor_completions.d
+          set --prepend --export fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d
+      end
+
+      if test -e $HOME/.nix-profile/share/fish/vendor_functions.d
+          set --prepend --export fish_function_path ~/.nix-profile/share/fish/vendor_functions.d
+      end
+
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+      end
+
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+      end
+
+      if test -e $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+        fenv source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+      end
+    '';
+
     interactiveShellInit = ''
       set -U __done_notification_urgency_level_failure normal
     '';
