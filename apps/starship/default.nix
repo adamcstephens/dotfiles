40d5cab597ad0acc068c47731af1689f4efe0aad
Adam Stephens <adam@valkor.net>
2023-03-09 15:47:07 -0500

    re-customize the starship someg

--- apps/starship/default.nix
+++ apps/starship/default.nix
@@ -1,5 +1,28 @@
 {...}: {
   programs.starship = {
     enable = true;
+
+    settings = {
+      character = {
+        format = "[─](fg:244)$symbol ";
+        success_symbol = "[❯](bold yellow)";
+        error_symbol = "[❯](bold red)";
+      };
+
+      gcloud = {
+        disabled = true;
+      };
+
+      hostname = {
+        style = "bold yellow";
+        format = "[$hostname]($style) ";
+        trim_at = "";
+      };
+
+      username = {
+        style_user = "fg:244";
+        format = "[$user@]($style)";
+      };
+    };
   };
 }
