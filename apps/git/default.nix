dd33266cadf2488ffb7eee1ed3b04fc0521cfab6
Adam Stephens <adam@valkor.net>
2023-03-09 14:51:27 -0500

    the lazy way

--- apps/git/default.nix
+++ apps/git/default.nix
@@ -2,27 +2,10 @@
   programs.git = {
     enable = true;
 
-    # aliases = {
-    #   ftag = "push --tags --force";
-    #   home = "!git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'); git pull";
-    #   fixhome = "remote set-head origin --auto";
-    #   subup = "submodule update --remote --merge --recursive";
-    #   yolo = "commit --amend --no-edit";
-    # };
-
     extraConfig = {
       include = {
         path = "${./gitconfig}";
       };
-      # color = {
-      #   ui = true;
-      # };
-      # "color \"diff\"" = {
-      #   meta = "blue";
-      #   frag = "magenta";
-      #   old = "red";
-      #   new = "green";
-      # };
     };
 
     ignores = [
