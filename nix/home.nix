32eb47200ca5469dd7dd157ff99817b9e2b57cc8
Adam Stephens <adam@valkor.net>
2023-03-10 16:13:49 -0500

    need to clone dotfiles still

--- nix/home.nix
+++ nix/home.nix
@@ -32,6 +32,19 @@
     };
   };
 
+  home.activation.dotfiles-bootstrap = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if [ ! -d ~/.dotfiles ]; then
+      ${lib.getExe pkgs.git} clone ${config.dotfiles.repo} ~/.dotfiles
+      touch ~/.dotfiles/.nixos-managed
+    fi
+
+    pushd ~/.dotfiles
+      if [ -e .nixos-managed ]; then
+        ${lib.getExe pkgs.git} pull
+      fi
+    popd
+  '';
+
   home.activation.directories = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
     for dir in git projects tmp; do
       if [ ! -d $HOME/$dir ]; then
