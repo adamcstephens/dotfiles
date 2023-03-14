5e4e588c3fc21b4c22251c6768cebaa23c65a505
Adam Stephens <adam@valkor.net>
2023-03-13 20:58:56 -0400

    disable puni in vterm

--- apps/emacs/dotemacs/elisp/init-lisp.el
+++ apps/emacs/dotemacs/elisp/init-lisp.el
@@ -7,6 +7,7 @@
   ;; The autoloads of Puni are set up so you can enable `puni-mode` or
   ;; `puni-global-mode` before `puni` is actually loaded. Only after you press
   ;; any key that calls Puni commands, it's loaded.
-  (puni-global-mode))
+  (puni-global-mode)
+  :hook (vterm-mode . puni-disable-puni-mode))
 
 (provide 'init-lisp)
