4114a79e808623171567783f17ee46534db0feeb
Adam Stephens <adam@valkor.net>
2023-03-16 19:54:09 -0400

    use builtin eglot

--- apps/emacs/dotemacs/elisp/init-lsp.el
+++ apps/emacs/dotemacs/elisp/init-lsp.el
@@ -1,11 +1,10 @@
-(use-package eglot
+(use-package
+  eglot
+  :straight (:type built-in)
   :defer t
   :ensure nil
   :commands (eglot eglot-ensure)
-  :hook
-  ((nix-mode . eglot-ensure))
-  :config
-  (add-to-list 'eglot-server-programs '((nix-mode) "nil"))
-  )
+  :hook ((nix-mode . eglot-ensure))
+  :config (add-to-list 'eglot-server-programs '((nix-mode) "nil")))
 
 (provide 'init-lsp)
