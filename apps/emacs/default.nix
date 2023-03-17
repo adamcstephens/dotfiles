9a3fd2f027b79c2b4c3fdb350be079522ac1cafc
Adam Stephens <adam@valkor.net>
2023-03-17 10:01:50 -0400

    force launchd to restart emacs on switch

--- apps/emacs/default.nix
+++ apps/emacs/default.nix
@@ -114,7 +114,7 @@ in {
           "${config.home.homeDirectory}/.nix-profile/bin/fish"
           "-l"
           "-c"
-          "emacs --fg-daemon --init-directory ${config.home.homeDirectory}/.config/emacs/dotemacs"
+          "${package}/bin/emacs --fg-daemon --init-directory ${config.home.homeDirectory}/.config/emacs/dotemacs"
         ];
         WatchPaths = [
           "${config.home.homeDirectory}/.nix-profile/bin/emacs"
