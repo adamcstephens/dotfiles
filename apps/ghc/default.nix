a5a10aac7898394b1d3050896becbc628036273b
Adam Stephens <adam@valkor.net>
2023-03-21 11:06:01 -0400

    improve ghci prompt

--- apps/ghc/default.nix
+++ apps/ghc/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   xdg.configFile."ghc/ghci.conf".text = ''
-    :set prompt "λ "
+    :set prompt "\n\SOH\ESC[90m\STX%s\SOH\ESC[0m\n\SOH\ESC[33m\STXλ\SOH\ESC[0m\STX > "
+    :set prompt-cont "\SOH\ESC[33m\STXλ+\SOH\ESC[0m\STX| "
   '';
 }
