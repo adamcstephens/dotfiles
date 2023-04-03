0caf475e4deaadb82c769b3eedf54d3ad6cfd39f
Adam Stephens <adam@valkor.net>
2023-04-03 19:24:20 -0400

    give up on workspace managers?

--- apps/emacs/dotemacs/elisp/init-project.el
+++ apps/emacs/dotemacs/elisp/init-project.el
@@ -31,10 +31,12 @@ project are filtered out."
   :straight (:type built-in)
   :init (setq project-switch-commands #'project-find-file)
   :bind
-  (("s-f" . project-find-file)
+  (("s-v" . project-find-file)
     ("s-u" . project-switch-project)
     ("C-c p p" . project-switch-project)
     ("C-c p d" . project-dired)))
+
+;; (use-package workroom)
 
 ;; (use-package
 ;;   project-tab-groups
@@ -49,38 +51,38 @@ project are filtered out."
 ;;   :ensure
 ;;   :config (project-mode-line-tag-mode 1))
 
-(use-package
-  persp-mode
-  :init (persp-mode 1)
-  :bind (("s-y" . persp-switch))
-  :custom (persp-kill-foreign-buffer-behaviour 'kill))
+;; (use-package
+;;   persp-mode
+;;   :init (persp-mode 1)
+;;   :bind (("s-y" . persp-switch))
+;;   :custom (persp-kill-foreign-buffer-behaviour 'kill))
 
-;; enable automatic project integration with persp
-(use-package
-  persp-mode-project-bridge
-  :after persp-mode
-  :hook
-  (persp-mode-project-bridge-mode
-    .
-    (lambda ()
-      (if persp-mode-project-bridge-mode
-        (persp-mode-project-bridge-find-perspectives-for-all-buffers)
-        (persp-mode-project-bridge-kill-perspectives))))
-  (persp-mode . persp-mode-project-bridge-mode)
-  :init (persp-mode-project-bridge-mode 1)
-  :config (setq persp-emacsclient-init-frame-behaviour-override "main"))
+;; ;; enable automatic project integration with persp
+;; (use-package
+;;   persp-mode-project-bridge
+;;   :after persp-mode
+;;   :hook
+;;   (persp-mode-project-bridge-mode
+;;     .
+;;     (lambda ()
+;;       (if persp-mode-project-bridge-mode
+;;         (persp-mode-project-bridge-find-perspectives-for-all-buffers)
+;;         (persp-mode-project-bridge-kill-perspectives))))
+;;   (persp-mode . persp-mode-project-bridge-mode)
+;;   :init (persp-mode-project-bridge-mode 1)
+;;   :config (setq persp-emacsclient-init-frame-behaviour-override "main"))
 
-(use-package
-  tabspaces
-  :disabled
-  :hook (after-init . tabspaces-mode)
-  :commands
-  (tabspaces-switch-or-create-workspace
-    tabspaces-open-or-create-project-and-workspace)
-  :custom
-  (tabspaces-use-filtered-buffers-as-default t)
-  (tabspaces-default-tab "main")
-  (tabspaces-remove-to-default t)
-  (tabspaces-include-buffers '("*scratch*")))
+;; (use-package
+;;   tabspaces
+;;   :disabled
+;;   :hook (after-init . tabspaces-mode)
+;;   :commands
+;;   (tabspaces-switch-or-create-workspace
+;;     tabspaces-open-or-create-project-and-workspace)
+;;   :custom
+;;   (tabspaces-use-filtered-buffers-as-default t)
+;;   (tabspaces-default-tab "main")
+;;   (tabspaces-remove-to-default t)
+;;   (tabspaces-include-buffers '("*scratch*")))
 
 (provide 'init-project)
