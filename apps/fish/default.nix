596f6b46f8d20a1c8f1024e61132c69cd7dd3d02
Adam Stephens <adam@valkor.net>
2023-04-04 11:37:50 -0400

    fish: split darwin/linux inits

--- apps/fish/default.nix
+++ apps/fish/default.nix
@@ -9,42 +9,52 @@
     enable = true;
     plugins = [];
 
-    shellInit = ''
-      if test -e $HOME/.nix-profile/share/fish/vendor_completions.d
-          set --prepend --export fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d
-      end
+    shellInit =
+      ''
+        fish_add_path --prepend --move ~/.dotfiles/bin
+      ''
+      + (
+        if pkgs.stdenv.isDarwin
+        then ''
+          if test -e $HOME/.nix-profile/share/fish/vendor_completions.d
+              set --prepend --export fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d
+          end
 
-      if test -e $HOME/.nix-profile/share/fish/vendor_functions.d
-          set --prepend --export fish_function_path ~/.nix-profile/share/fish/vendor_functions.d
-      end
+          if test -e $HOME/.nix-profile/share/fish/vendor_functions.d
+              set --prepend --export fish_function_path ~/.nix-profile/share/fish/vendor_functions.d
+          end
 
-      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-      end
+          if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+            fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+          end
 
-      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-      end
+          if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+            fenv source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+          end
 
-      if test -e $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
-        fenv source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
-      end
+          if test -e $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+            fenv source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+          end
 
-      if test -x /opt/homebrew/bin/brew
-          # eval (/opt/homebrew/bin/brew shellenv)
-          # do this manually to avoid homebrew overriding nix
-          set -gx HOMEBREW_PREFIX /opt/homebrew
-          set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
-          set -gx HOMEBREW_REPOSITORY /opt/homebrew
-          set --append -gx PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
-          set --append -gx MANPATH /opt/homebrew/share/man $MANPATH
-          set --append -gx INFOPATH /opt/homebrew/share/info $INFOPATH
-      end
-
-      fish_add_path --prepend --move ~/.dotfiles/bin
-    '';
+          if test -x /opt/homebrew/bin/brew
+              # eval (/opt/homebrew/bin/brew shellenv)
+              # do this manually to avoid homebrew overriding nix
+              set -gx HOMEBREW_PREFIX /opt/homebrew
+              set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
+              set -gx HOMEBREW_REPOSITORY /opt/homebrew
+              set --append -gx PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+              set --append -gx MANPATH /opt/homebrew/share/man $MANPATH
+              set --append -gx INFOPATH /opt/homebrew/share/info $INFOPATH
+          end
+        ''
+        else ""
+      );
 
     interactiveShellInit = ''
+       if [ (uname) = Darwin ] && [ -e "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh" ]
+         set -x SSH_AUTH_SOCK "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh"
+       end
+
        set -U __done_notification_urgency_level_failure normal
        set -U fish_greeting
 
