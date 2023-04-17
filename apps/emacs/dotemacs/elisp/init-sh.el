b84c8bd36b6b3272e4300aed44e665f4dd2a2db2
Adam Stephens <adam@valkor.net>
2023-04-16 21:37:10 -0400

    use bash-ts-mode and nushell-mode

--- apps/emacs/dotemacs/elisp/init-sh.el
+++ apps/emacs/dotemacs/elisp/init-sh.el
@@ -1,4 +1,11 @@
+(use-package bash-ts-mode :straight (:type built-in) :mode "\\.sh\\'")
+
 (use-package fish-mode :mode ("\\.fish\\'" . fish-mode))
+
+(use-package
+  nushell-mode
+  :straight (:type git :host github :repo "azzamsa/emacs-nushell")
+  :mode "\\.nu\\'")
 
 (use-package
   sh-script
