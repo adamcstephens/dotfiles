8807f659fc26ba8f7a90ee10ad3b871ef97968df
Adam Stephens <adam@valkor.net>
2023-05-05 07:53:35 -0400

    commit unused xob to get it out of git status

---
+++ apps/xob/default.nix
@@ -0,0 +1,68 @@
+{config, ...}: {
+  imports = [
+    ./hm-module.nix
+  ];
+
+  services.xob = {
+    enable = true;
+    styles = {
+      default = {
+        x = {
+          relative = 1;
+          offset = -48;
+        };
+        y = {
+          relative = 0.5;
+          offset = 0;
+        };
+        length = {
+          relative = 0.3;
+          offset = 0;
+        };
+        thickness = 24;
+        outline = 3;
+        border = 4;
+        padding = 3;
+        orientation = "vertical";
+
+        overflow = "proportional";
+
+        color = {
+          normal = {
+            fg = "#ffffff";
+            bg = "#00000090";
+            border = "#ffffff";
+          };
+          alt = {
+            fg = "#555555";
+            bg = "#00000090";
+            border = "#555555";
+          };
+          overflow = {
+            fg = "#ff0000";
+            bg = "#00000090";
+            border = "#ff0000";
+          };
+          altoverflow = {
+            fg = "#550000";
+            bg = "#00000090";
+            border = "#550000";
+          };
+        };
+      };
+    };
+    # settings = {
+    #   globalSection = {
+    #     width = 256;
+    #     height = 40;
+    #     margin = 40;
+    #     anchor = "bottom right";
+    #     background_color = "${config.colorScheme.colors.base00}";
+    #     border_color = "${config.colorScheme.colors.base03}";
+    #     bar_color = "${config.colorScheme.colors.base09}";
+    #   };
+    #   sections = {};
+    # };
+    systemdTarget = "xserver-session.target";
+  };
+}
---
+++ apps/xob/hm-module.nix
@@ -0,0 +1,69 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.xob;
+
+  generate = lib.generators.toPretty {};
+  stylesCfg = builtins.toString (builtins.map (name: "${name} = ${generate cfg.styles.${name}};\n") (builtins.attrNames cfg.styles));
+
+  script = pkgs.writeScript "xob-script" ''
+    #!${lib.getExe pkgs.bash}
+
+    ${pkgs.coreutils}/bin/tail -F $XDG_RUNTIME_DIR/xob.sock | ${pkgs.xob}/bin/xob
+  '';
+in {
+  options.services.xob = {
+    enable = lib.mkEnableOption "X overlay bar";
+
+    styles = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+      description = "Styles configuration for xob";
+    };
+
+    systemdTarget = lib.mkOption {
+      type = lib.types.str;
+      default = "graphical-session.target";
+      example = "xserver-session.target";
+      description = ''
+        The systemd target that will automatically start the service.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    assertions = [
+      (lib.hm.assertions.assertPlatform "services.xob" pkgs lib.platforms.linux)
+    ];
+
+    xdg.configFile."xob/styles.cfg".text = stylesCfg;
+
+    systemd.user.services.xob = {
+      Unit = {
+        Description = "X overlay bar";
+        PartOf = [cfg.systemdTarget];
+      };
+
+      Install.WantedBy = [cfg.systemdTarget];
+
+      Service = {
+        ExecStart = script.outPath;
+        StandardInput = "socket";
+        Restart = "on-failure";
+        RestartSec = 1;
+      };
+    };
+
+    systemd.user.sockets.xob = {
+      Socket = {
+        ListenFIFO = "%t/xob.sock";
+        SocketMode = 0600;
+      };
+
+      Install.WantedBy = [cfg.systemdTarget];
+    };
+  };
+}
