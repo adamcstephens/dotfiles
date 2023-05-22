9641e9b858b1f6f8278cb8b7bb5d2fe85682e573
Adam Stephens <adam@valkor.net>
2023-05-22 10:45:22 -0400

    don't use variable-pitch-mode in heex-ts-mode

--- apps/emacs/dotemacs/elisp/init-elixir.el
+++ apps/emacs/dotemacs/elisp/init-elixir.el
@@ -3,4 +3,8 @@
   ;; only load mode with elixir files
   :mode "\\.exs?\\'")
 
+(use-package
+  heex-ts-mode
+  :hook (heex-ts-mode . (lambda () (variable-pitch-mode -1))))
+
 (provide 'init-elixir)
