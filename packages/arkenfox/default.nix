542fbb5a22a84db1002dace7a6027b72b6fe8b44
Adam Stephens <adam@valkor.net>
2023-08-28 15:32:08 -0400

    refactor arkenfox package

---
+++ packages/arkenfox/default.nix
@@ -0,0 +1,58 @@
+{
+  stdenvNoCC,
+  fetchFromGitHub,
+  makeWrapper,
+  writeScript,
+}:
+stdenvNoCC.mkDerivation rec {
+  pname = "arkenfox";
+  version = "115.0";
+
+  src = fetchFromGitHub {
+    owner = "arkenfox";
+    repo = "user.js";
+    rev = "refs/tags/${version}";
+    hash = "sha256-knJa1zI27NsKGwpps3MMrG9K7HDGCDnoRfm16pNR/yM=";
+  };
+
+  dontPatch = true;
+  dontBuild = true;
+  dontConfigure = true;
+  dontFixup = true;
+
+  buildInputs = [makeWrapper];
+
+  installPhase = let
+    script = writeScript "arkenfox-patch" ''
+      set -e
+      if [ "$(uname)" = "Darwin" ]; then
+        PROFILES_DIR="$HOME/Library/Application Support/Firefox/Profiles/"
+      elif [ "$(uname)" = "Linux" ]; then
+        PROFILES_DIR="$HOME/.mozilla/firefox/"
+      fi
+      if pgrep -i firefox; then
+        echo "!! Quit all Firefox processes first !!"
+        exit 1
+      fi
+      for FIREFOX_PROFILE in "$PROFILES_DIR"*; do
+        if [ ! -e "$FIREFOX_PROFILE/cookies.sqlite" ]; then
+          continue
+        fi
+        echo ":: Arkenfoxing: $FIREFOX_PROFILE ::"
+        cd "$FIREFOX_PROFILE" || exit 1
+        cp ${src}/prefsCleaner.sh .
+        cp ${src}/updater.sh .
+        cp ${src}/user.js .
+        chmod +w ./prefsCleaner.sh ./updater.sh ./user.js
+        ln -sf ~/.dotfiles/apps/firefox/user-overrides.js .
+        ./updater.sh -s
+        bash ./prefsCleaner.sh -s
+      done
+    '';
+  in ''
+    mkdir -p $out/bin
+    makeWrapper ${script} $out/bin/arkenfox-patch
+  '';
+
+  meta.mainProgram = "arkenfox-patch";
+}
--- packages/default.nix
+++ packages/default.nix
@@ -3,14 +3,7 @@
   homeConfigurations,
   ...
 }: rec {
-  arkenfox-patch = pkgs.callPackage ./arkenfox-patch.nix {
-    arkenfoxSrc = pkgs.fetchFromGitHub {
-      owner = "arkenfox";
-      repo = "user.js";
-      rev = "115.0";
-      hash = "sha256-knJa1zI27NsKGwpps3MMrG9K7HDGCDnoRfm16pNR/yM=";
-    };
-  };
+  arkenfox = pkgs.callPackage ./arkenfox {};
   hm = pkgs.callPackage ./hm.nix {
     inherit home-profile-selector;
   };
