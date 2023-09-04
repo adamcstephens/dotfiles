d7bfbfbcef88bb94eae084035173ae3a3988486a
Adam Stephens <adam@valkor.net>
2023-09-03 23:17:40 -0400

    chore: lsd changed theme format

--- apps/lsd/default.nix
+++ apps/lsd/default.nix
@@ -1,11 +1,13 @@
 {...}: {
+  xdg.configFile."lsd/colors.yaml".source = ./themes/terminal.yaml;
+
   programs.lsd = {
     enable = true;
     enableAliases = true;
     settings = {
       color = {
         when = "auto";
-        theme = ./themes/terminal.yaml;
+        theme = "custom";
       };
     };
   };
