37b3e8f24698eb3d50005c57fd96e85e9cbf7b22
Adam Stephens <adam@valkor.net>
2023-09-08 23:52:57 -0400

    try: more agressively use follows

--- flake.nix
+++ flake.nix
@@ -4,17 +4,22 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     apple-fonts.url = "git+https://codeberg.org/adamcstephens/apple-fonts.nix";
+    apple-fonts.inputs.nixpkgs.follows = "nixpkgs";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
     emacs-plus.url = "github:d12frosted/homebrew-emacs-plus";
     emacs-plus.flake = false;
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nil.url = "github:oxalica/nil";
+    nil.inputs.nixpkgs.follows = "nixpkgs";
     nix-colors.url = "github:misterio77/nix-colors";
+    nix-colors.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:lnl7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nix-wallpaper.url = "github:lunik1/nix-wallpaper";
     nix-wallpaper.inputs.nixpkgs.follows = "nixpkgs";
     nixfmt-rfc.url = "github:piegamesde/nixfmt/rfc101-style";
