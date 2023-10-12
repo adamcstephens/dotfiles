0260e133c3c07c46381bad2b3b005331d2ff938a
Adam Stephens <adam@valkor.net>
2023-10-12 15:35:53 -0400

    try: think back to river master

--- home/profiles.nix
+++ home/profiles.nix
@@ -104,16 +104,16 @@ in {
 
         ({pkgs, ...}: {
           dotfiles = {
-            # apps.river.package = pkgs.river.overrideAttrs (_: rec {
-            #   version = "0.3.0-${builtins.substring 0 7 src.rev}";
-            #   src = pkgs.fetchFromGitHub {
-            #     owner = "riverwm";
-            #     repo = "river";
-            #     rev = "7f30c655c75568ae331ed0243578d91870f3f9c6";
-            #     fetchSubmodules = true;
-            #     hash = "sha256-7cYLP2FID/DW4A06/Ujtqp2LE7NlHwaymQLiIA8xrMk=";
-            #   };
-            # });
+            apps.river.package = pkgs.river.overrideAttrs (_: rec {
+              version = "0.3.0-${builtins.substring 0 7 src.rev}";
+              src = pkgs.fetchFromGitHub {
+                owner = "riverwm";
+                repo = "river";
+                rev = "7f30c655c75568ae331ed0243578d91870f3f9c6";
+                fetchSubmodules = true;
+                hash = "sha256-7cYLP2FID/DW4A06/Ujtqp2LE7NlHwaymQLiIA8xrMk=";
+              };
+            });
             gui.wayland = true;
           };
           programs.waybar.settings.main.output = ["eDP-1" "DP-2"];
