3c1c0a323bb7964b686ba9ae4f55a20441aacfe5
Adam Stephens <adam@valkor.net>
2023-12-30 16:46:20 -0500

    cleanup: gitconfig

--- apps/git/default.nix
+++ apps/git/default.nix
@@ -12,19 +12,10 @@
     ignores = [
       "*.log"
       "*.retry"
-      "*.sw?"
-      ".bundle"
-      ".ruby-version"
-      ".notags"
-      ".tags"
-      ".tags.lock"
       ".DS_Store"
-      ".elixir_ls/"
-      "result"
       ".direnv/"
-      ".calva/"
-      ".clj-kondo/"
       ".lsp/"
+      "result"
     ];
   };
 }
