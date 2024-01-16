8b8db6c45a6cbd8bd54427070927f992b63888d2
Adam Stephens <adam@valkor.net>
2024-01-16 09:20:22 -0500

    fix: init to insert mode on commit

--- apps/emacs/elisp/init-git.el
+++ apps/emacs/elisp/init-git.el
@@ -2,7 +2,8 @@
   magit
   :commands git-commit-mode
   :bind (("C-c g" . magit-file-dispatch))
-  :hook (focus-in . magit-refresh)
+  :hook ((focus-in . magit-refresh)
+         (git-commit-mode . evil-insert-state))
   :config (setq magit-save-repository-buffers nil))
 
 ;; (use-package forge :after magit)
@@ -15,11 +16,7 @@
   (diff-hl-margin-mode)
   ;; (diff-hl-flydiff-mode)
   :hook
-  (diff-hl-mode-on
-    .
-    (lambda ()
-      (unless (window-system)
-        (diff-hl-margin-local-mode))))
+  (diff-hl-mode-on . (lambda () (unless (window-system) (diff-hl-margin-local-mode))))
   ('magit-pre-refresh . diff-hl-magit-pre-refresh)
   ('magit-post-refresh . diff-hl-magit-post-refresh))
 
@@ -32,25 +29,25 @@
 
 ;; load diff mode on cli commit
 (add-to-list
-  'auto-mode-alist
-  '("/COMMIT_EDITMSG\\'" . git-commit-mode))
+ 'auto-mode-alist
+ '("/COMMIT_EDITMSG\\'" . git-commit-mode))
 
 ;; tweak the vc output in the mode line
 (advice-add
-  #'vc-git-mode-line-string
-  :filter-return #'dot/replace-git-status)
+ #'vc-git-mode-line-string
+ :filter-return #'dot/replace-git-status)
 (defun dot/replace-git-status (tstr)
   (let*
-    (
-      (tstr (replace-regexp-in-string "Git" "" tstr))
-      (first-char (substring tstr 0 1))
-      (rest-chars (substring tstr 1)))
+      (
+       (tstr (replace-regexp-in-string "Git" "" tstr))
+       (first-char (substring tstr 0 1))
+       (rest-chars (substring tstr 1)))
     (cond
-      ((string= ":" first-char) ;;; Modified
-        (replace-regexp-in-string "^:" "!" tstr))
-      ((string= "-" first-char) ;; No change
-        (replace-regexp-in-string "^-" "" tstr))
-      (t
-        tstr))))
+     ((string= ":" first-char) ;;; Modified
+      (replace-regexp-in-string "^:" "!" tstr))
+     ((string= "-" first-char) ;; No change
+      (replace-regexp-in-string "^-" "" tstr))
+     (t
+      tstr))))
 
 (provide 'init-git)
