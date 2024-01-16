0ee53fb936cb94d709bfb43d25f6a9075c6346f8
Adam Stephens <adam@valkor.net>
2024-01-16 10:52:10 -0500

    cleanup: unmanage vscode

--- apps/vscodium/default.nix
+++ apps/vscodium/default.nix
@@ -32,21 +32,15 @@ let
   prefix = if pkgs.stdenv.isDarwin then "Library/Application Support" else ".config";
 in
 {
-  home.file."${prefix}/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/vscodium/keybindings.json";
-  home.file."${prefix}/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/vscodium/settings.json";
   home.file."${prefix}/VSCodium/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/vscodium/keybindings.json";
   home.file."${prefix}/VSCodium/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/vscodium/settings.json";
 
   # they say you shouldn't modify the system in this phase, but... 🤷‍♂️
   home.activation.own-vscode-snippets = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-    if [ ! -h "${config.home.homeDirectory}/${prefix}/Code/User/snippets" ]; then
-      rm -rfv "${config.home.homeDirectory}/${prefix}/Code/User/snippets"
-    fi
     if [ ! -h "${config.home.homeDirectory}/${prefix}/VSCodium/User/snippets" ]; then
       rm -rfv "${config.home.homeDirectory}/${prefix}/VSCodium/User/snippets"
     fi
   '';
-  home.file."${prefix}/Code/User/snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/vscodium/snippets";
   home.file."${prefix}/VSCodium/User/snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/vscodium/snippets";
 
   programs.vscode = {
--- bin/vscodium-extensions.sh
+++ bin/vscodium-extensions.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-for code in codium code code-insiders; do
-  EXTENSIONS_FILE="$HOME/.dotfiles/apps/vscodium/$code-extensions.txt"
-  UNINSTALL_FILE="$HOME/.dotfiles/apps/vscodium/$code-extensions-uninstall.txt"
+for codium in codium; do
+  EXTENSIONS_FILE="$HOME/.dotfiles/apps/vscodium/$codium-extensions.txt"
+  UNINSTALL_FILE="$HOME/.dotfiles/apps/vscodium/$codium-extensions-uninstall.txt"
 
-  if command -v $code >/dev/null 2>&1; then
-    case "$(which $code)" in
+  if command -v $codium >/dev/null 2>&1; then
+    case "$(which $codium)" in
     *bin/remote-cli/*)
       exit 0
       ;;
@@ -15,11 +15,11 @@ for code in codium code code-insiders; do
         TMPFILE="$(mktemp)"
         sed -e "/$2/Id" "$UNINSTALL_FILE" | sort -u >"$TMPFILE"
         mv "$TMPFILE" "$UNINSTALL_FILE"
-        $code --install-extension "$2"
+        $codium --install-extension "$2"
         ;;
       remove)
         sed -i "/$2/Id" "$EXTENSIONS_FILE"
-        $code --uninstall-extension "$2"
+        $codium --uninstall-extension "$2"
         rc=$?
         TMPFILE="$(mktemp)"
         echo "$2" >>"$UNINSTALL_FILE"
@@ -29,25 +29,25 @@ for code in codium code code-insiders; do
         ;;
       esac
 
-      installed="$($code --list-extensions 2>/dev/null)"
+      installed="$($codium --list-extensions 2>/dev/null)"
       repo="$(cat "$EXTENSIONS_FILE")"
       uninstall="$(cat "$UNINSTALL_FILE")"
 
       for u in $uninstall; do
         ext=$(echo "$u" | sed -r 's/[[:space:]]+//g')
         if echo "$installed" | grep "$ext" >/dev/null; then
-          $code --uninstall-extension "$ext"
+          $codium --uninstall-extension "$ext"
         fi
       done
 
       for e in $repo; do
         ext=$(echo "$e" | sed -r 's/[[:space:]]+//g')
         if ! (echo "$installed" | grep "$ext" >/dev/null); then
-          $code --install-extension "$ext"
+          $codium --install-extension "$ext"
         fi
       done
 
-      $code --list-extensions >"$EXTENSIONS_FILE" 2>/dev/null
+      $codium --list-extensions >"$EXTENSIONS_FILE" 2>/dev/null
       ;;
     esac
   fi
