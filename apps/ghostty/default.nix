e1ac8142b2ef429a63961d5ad7773faa62553cbe
Adam Stephens <adam@valkor.net>
2024-01-25 19:54:37 -0500

    fix: ghostty per-system

--- apps/ghostty/default.nix
+++ apps/ghostty/default.nix
@@ -1,6 +1,18 @@
-{ ... }:
 {
-  xdg.configFile."ghostty/config".text = ''
-    window-decoration = false
-  '';
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  xdg.configFile."ghostty/config".text =
+    ''
+      font-family = "${config.dotfiles.gui.font.mono}"
+    ''
+    + lib.optionalString pkgs.stdenv.isLinux ''
+      gtk-single-instance = true
+      window-decoration = false
+
+      font-size = 11
+    '';
 }
