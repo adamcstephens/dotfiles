640a73dd4796b8aba6fedcfe83517a6bfaad39eb
Adam Stephens <adam@valkor.net>
2024-02-12 08:52:24 -0500

    better folding

--- apps/neovim/init.lua
+++ apps/neovim/init.lua
@@ -87,8 +87,10 @@ vim.opt.undofile   = true
 vim.opt.whichwrap  = "<,>,[,]"
 
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr   = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr   = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext   = "v:lua.vim.treesitter.foldtext()"
 vim.cmd('set nofoldenable')
+
 
 -- mappings
 --
