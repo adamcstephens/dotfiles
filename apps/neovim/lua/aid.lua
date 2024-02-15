2dad74e508daa4ee4cd5b66fd885df4d9d1d63a0
Adam Stephens <adam@valkor.net>
2024-02-15 10:59:42 -0500

    pref: prettier hovers

--- apps/neovim/lua/aid.lua
+++ apps/neovim/lua/aid.lua
@@ -1,3 +1,19 @@
+-- builtin
+vim.diagnostic.config({
+  float = {
+    border = 'rounded',
+  },
+})
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = 'rounded' }
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  { border = 'rounded' }
+)
+
 -- cmp
 --
 local cmp = require('cmp')
