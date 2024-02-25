8c2aebcede8fbd2288363c50f783453d6e235983
Adam Stephens <adam@valkor.net>
2024-02-25 08:23:18 -0500

    fixup! reset zellij

--- apps/zellij/default.nix
+++ apps/zellij/default.nix
@@ -3,5 +3,8 @@
   home.packages = [ pkgs.zellij ];
 
   home.file.".config/zellij".source =
-    if config.dotfiles.nixosManaged then ./. else "${config.home.homeDirectory}/.dotfiles/apps/zellij";
+    if config.dotfiles.nixosManaged then
+      ./.
+    else
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/zellij";
 }
