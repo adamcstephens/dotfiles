93e336c372b8b0d3ab069c21fdc6570c4d5d8d73
Adam Stephens <adam@valkor.net>
2024-03-10 23:09:43 -0400

    example tmux plugin

--- apps/tmux/default.nix
+++ apps/tmux/default.nix
@@ -1,9 +1,16 @@
-{ npins, ... }:
+{ npins, pkgs, ... }:
 {
   programs.tmux = {
     enable = true;
     extraConfig =
       builtins.readFile "${npins."modus-themes.nvim"}/extras/tmux/modus_vivendi.tmux"
       + builtins.readFile ./tmux.conf;
+
+    # plugins = [
+    #   {
+    #     plugin = pkgs.tmuxPlugins.tmux-fzf;
+    #     extraConfig = "set -g @plugin 'sainnhe/tmux-fzf'";
+    #   }
+    # ];
   };
 }
