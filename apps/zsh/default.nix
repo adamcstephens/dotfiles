9013ebf0bbd5fa34da9d599b058487d6ce459606
Adam Stephens <adam@valkor.net>
2024-03-20 00:05:01 -0400

    i don't even know sometimes

--- apps/zsh/default.nix
+++ apps/zsh/default.nix
@@ -2,7 +2,7 @@
 {
   programs.zsh = {
     enable = true;
-    autosuggestions.enable = true;
+    enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
 
