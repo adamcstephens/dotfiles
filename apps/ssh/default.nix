40780e07bf11372c5dc18ad42aad810a76dd9c71
Adam Stephens <adam@valkor.net>
2024-03-27 12:32:01 -0400

    add ssh-tpm-agent support, but don't use it

--- apps/ssh/default.nix
+++ apps/ssh/default.nix
@@ -1,13 +1,40 @@
-{ ... }:
 {
-  programs.ssh = {
-    enable = true;
-    controlMaster = "auto";
-    controlPersist = "600m";
-    serverAliveInterval = 60;
-    serverAliveCountMax = 2;
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.apps.ssh;
+in
+{
+  options.apps.ssh.tpm = lib.mkEnableOption "ssh-tpm-agent";
 
-    # use header: # -*- mode: ssh-config -*-
-    includes = [ "local.config" ];
+  config = {
+    home.packages = lib.optionals cfg.tpm [ pkgs.ssh-tpm-agent ];
+
+    programs.ssh = {
+      enable = true;
+      controlMaster = "auto";
+      controlPersist = "600m";
+      serverAliveInterval = 60;
+      serverAliveCountMax = 2;
+
+      # use header: # -*- mode: ssh-config -*-
+      includes = [ "local.config" ];
+    };
+
+    systemd.user.services.ssh-tpm-agent = lib.mkIf cfg.tpm {
+      Unit.PartOf = [ "default.target" ];
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${lib.getExe pkgs.ssh-tpm-agent} -l %t/ssh-agent";
+        RestartSec = 3;
+        Restart = "on-abort";
+      };
+
+      Install.WantedBy = [ "default.target" ];
+    };
   };
 }
