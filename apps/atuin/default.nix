bc868f54848322e16d70d01cf8ec95a15797a93d
Adam Stephens <adam@valkor.net>
2024-04-22 09:49:27 -0400

    patch atuin for zfs and configure more

--- apps/atuin/default.nix
+++ apps/atuin/default.nix
@@ -1,10 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.atuin = {
     enable = true;
+    package = pkgs.atuin.overrideAttrs (old: {
+      patches = [
+        # https://github.com/Mic92/dotfiles/blob/main/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch
+        (pkgs.fetchpatch {
+          url = "https://github.com/Mic92/dotfiles/raw/c2f538934d67417941f83d8bb65b8263c43d32ca/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch";
+          hash = "sha256-i0kBQPr/oubW3i/BaAXx2CQx2OMN+iIAIGhz60+Qft8=";
+        })
+      ];
+    });
 
     settings = {
       enter_accept = false;
+      filter_mode = "directory";
+      inline_height = 30;
+      style = "compact";
       update_check = false;
     };
   };
