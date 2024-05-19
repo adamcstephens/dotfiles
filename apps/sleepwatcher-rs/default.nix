fbadbf9d0ae8f443a21acb3a68573e12b2f117be
Adam Stephens <adam@valkor.net>
2024-05-19 09:04:24 -0400

    init sleepwatcher

---
+++ apps/sleepwatcher-rs/default.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  colors = config.colorScheme.palette;
+  locker = pkgs.writeShellScriptBin "locker" ''
+    exec ${lib.getExe pkgs.waylock} -fork-on-lock -init-color 0x${colors.base01} -input-color 0x${colors.base03} -fail-color 0x${colors.base08}";
+  '';
+in
+{
+  imports = [ inputs.sandbox.homeModules.sleepwatcher-rs ];
+
+  services.sleepwatcher-rs = {
+    enable = true;
+
+    configFile =
+      if config.dotfiles.nixosManaged then
+        ./idle_config.lua
+      else
+        config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/sleepwatcher-rs/idle_config.lua";
+
+    dependencies = [
+      "/run/current-system/sw"
+      locker
+      pkgs.playerctl
+      pkgs.wlopm
+    ];
+
+    systemdTarget = "wayland-session.target";
+  };
+}
---
+++ apps/sleepwatcher-rs/idle_config.lua
@@ -0,0 +1,71 @@
+Helpers:log("Loading idle_config.lua")
+
+function LockScreen()
+  Helpers:log("Locking Screen")
+  IdleNotifier:run_once("locker")
+end
+
+function LockHandler()
+  IdleNotifier:run("playerctl -a pause")
+  LockScreen()
+end
+
+function DpmsOn()
+  Helpers:log("Turning screen on")
+  IdleNotifier:run("wlopm --on '*'")
+end
+
+function DpmsOff()
+  Helpers:log("Turning screen off")
+  IdleNotifier:run("wlopm --off '*'")
+end
+
+function Suspend()
+  IdleNotifier:run("systemctl suspend")
+end
+
+function ScreenLockBattery(event)
+  if event == "idled" and Helpers:on_battery() then
+    LockScreen()
+  end
+end
+
+function SleepBattery(event)
+  if event == "idled" and Helpers:on_battery() then
+    Suspend()
+  end
+end
+
+function ScreenDpmsBattery(event)
+  -- Resumed should always be executed
+  if event == "resumed" then
+    DpmsOn()
+  elseif event == "idled" and Helpers:on_battery() then
+    DpmsOff()
+  end
+end
+
+function ScreenLockAC(event)
+  if event == "idled" and not Helpers:on_battery() then
+    LockScreen()
+  end
+end
+
+function ScreenDpmsAC(event)
+  if event == "resumed" then
+    DpmsOn()
+  elseif event == "idled" and not Helpers:on_battery() then
+    DpmsOff()
+  end
+end
+
+DbusHandler:PrepareSleep("LockScreen")
+DbusHandler:LockHandler("LockHandler")
+-- DbusHandler:UnlockHandler("UnlockHandler")
+IdleNotifier:get_notification(300, "ScreenLockBattery")
+IdleNotifier:get_notification(30, "ScreenDpmsBattery")
+IdleNotifier:get_notification(600, "SleepBattery")
+IdleNotifier:get_notification(600, "ScreenLockAC")
+IdleNotifier:get_notification(900, "ScreenDpmsAC")
+
+Helpers:log("Finished loading idle_config.lua")
