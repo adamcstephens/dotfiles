aaa9054ab8b89e421c4d9d071cc9e1c2401a582b
Adam Stephens <adam@valkor.net>
2024-05-22 15:35:24 -0400

    format javascript

--- apps/neovim/lua/aid.lua
+++ apps/neovim/lua/aid.lua
@@ -40,6 +40,7 @@ require("conform").setup({
   },
   formatters_by_ft = {
     elixir = { "mix" },
+    javascript = { "biome" },
     json = { "biome" },
     lua = { "stylua" },
     nix = { "nixfmt" },
