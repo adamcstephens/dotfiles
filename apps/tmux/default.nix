ff6a51501d5b0d22b3204727fa177da0174f9289
Adam Stephens <adam@valkor.net>
2024-05-23 10:32:19 -0400

    init tmuxinator and a config for blank

--- apps/tmux/default.nix
+++ apps/tmux/default.nix
@@ -1,16 +1,22 @@
-{ npins, pkgs, ... }:
 {
+  config,
+  npins,
+  pkgs,
+  ...
+}:
+{
+  home.packages = [ pkgs.tmuxinator ];
+
+  xdg.configFile.tmuxinator.source =
+    if config.dotfiles.nixosManaged then
+      ./tmuxinator
+    else
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/tmux/tmuxinator";
+
   programs.tmux = {
     enable = true;
     extraConfig =
       builtins.readFile "${npins."modus-themes.nvim"}/extras/tmux/modus_vivendi.tmux"
       + builtins.readFile ./tmux.conf;
-
-    # plugins = [
-    #   {
-    #     plugin = pkgs.tmuxPlugins.tmux-fzf;
-    #     extraConfig = "set -g @plugin 'sainnhe/tmux-fzf'";
-    #   }
-    # ];
   };
 }
