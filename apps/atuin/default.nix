06e5a233148861fabe1f04c991b49085a5aef94a
Adam Stephens <adam@valkor.net>
2024-06-14 16:36:26 -0400

    fix: stop patching atuin for newer versions

--- apps/atuin/default.nix
+++ apps/atuin/default.nix
@@ -1,16 +1,20 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   programs.atuin = {
     enable = true;
-    package = pkgs.atuin.overrideAttrs (old: {
-      patches = [
-        # https://github.com/Mic92/dotfiles/blob/main/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch
-        (pkgs.fetchpatch {
-          url = "https://github.com/Mic92/dotfiles/raw/c2f538934d67417941f83d8bb65b8263c43d32ca/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch";
-          hash = "sha256-i0kBQPr/oubW3i/BaAXx2CQx2OMN+iIAIGhz60+Qft8=";
-        })
-      ];
-    });
+    package =
+      if (lib.versionAtLeast pkgs.atuin.version "18.3.0") then
+        pkgs.atuin
+      else
+        pkgs.atuin.overrideAttrs (old: {
+          patches = [
+            # https://github.com/Mic92/dotfiles/blob/main/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch
+            (pkgs.fetchpatch {
+              url = "https://github.com/Mic92/dotfiles/raw/c2f538934d67417941f83d8bb65b8263c43d32ca/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch";
+              hash = "sha256-i0kBQPr/oubW3i/BaAXx2CQx2OMN+iIAIGhz60+Qft8=";
+            })
+          ];
+        });
 
     flags = [ "--disable-up-arrow" ];
 
