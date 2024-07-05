75121d8aa13af27952b4ca0aa1d034777587a25e
Adam Stephens <adam@valkor.net>
2024-07-05 13:36:13 -0400

    add luasnip jump

--- apps/neovim/lua/dotinit.lua
+++ apps/neovim/lua/dotinit.lua
@@ -251,3 +251,17 @@ end)
 vim.keymap.set("n", "<leader>y", builtin.registers, { desc = "Registers" })
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
+
+local ls = require("luasnip")
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+  ls.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-K>", function()
+  ls.jump(-1)
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, { silent = true })
