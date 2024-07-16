1ef78b6d42733e4a63c0bc8cc93869efaa603f1f
Adam Stephens <adam@valkor.net>
2024-07-16 09:33:50 -0400

    stop purging cookies/sessions and avoid warn on quit

--- apps/firefox/user-overrides.js
+++ apps/firefox/user-overrides.js
@@ -3,6 +3,8 @@ user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("browser.tabs.firefox-view", false);
 // * 0=blank, 1=home, 2=last visited page, 3=resume previous session
 user_pref("browser.startup.page", 3);
+user_pref("browser.warnOnQuit", false);
+user_pree("browser.warnOnQuitShortcut", false);
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 user_pref("extensions.formautofill.heuristics.enabled", false);
@@ -11,6 +13,9 @@ user_pref("geo.enabled", false);
 user_pref("keyword.enabled", true);
 user_pref("network.trr.mode", 5);
 user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
 user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("privacy.resistFingerprinting", false);
 user_pref("signon.rememberSignons", false);
