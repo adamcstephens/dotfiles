9161272fd8ddf6b81a0cf20f0ae00ac252314163
Adam Stephens <adam@valkor.net>
2024-08-16 10:06:32 -0400

    linux-gui: hyprland default resolution and force tiling for file uploads

--- apps/river/init.sh
+++ apps/river/init.sh
@@ -214,7 +214,7 @@ riverctl rule-add -title '*Element*' tags $((2#00100000))
 riverctl rule-add -title '*Discord*' tags $((2#00100000))
 
 riverctl rule-add -app-id Kid3 no-float
-riverctl rule-add -title "Open File" no-float
+riverctl rule-add -title "*File*" no-float
 
 # warp the mouse
 riverctl set-cursor-warp on-focus-change
