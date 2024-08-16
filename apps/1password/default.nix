8bf3dd30edaf8644d0225ae9ac479ebee8406411
Adam Stephens <adam@valkor.net>
2024-08-16 09:47:30 -0400

    1password: start after waybar

--- apps/1password/default.nix
+++ apps/1password/default.nix
@@ -3,6 +3,9 @@
   systemd.user.services."1password" = {
     Install.WantedBy = [ "graphical-session.target" ];
     Service.ExecStart = "${lib.getExe pkgs._1password-gui} --silent";
-    Unit.After = [ "graphical-session.target" ];
+    Unit.After = [
+      "graphical-session.target"
+      "waybar.service"
+    ];
   };
 }
