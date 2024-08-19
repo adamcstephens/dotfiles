8f4a5b05e010a87aaf19ffc0b961c086720f9e6b
Adam Stephens <adam@valkor.net>
2024-08-18 20:34:27 -0400

    fish: async prompt runs in non-interactive, move colors there

--- apps/fish/interactive.fish
+++ apps/fish/interactive.fish
@@ -30,10 +30,6 @@ end
 abbr --add gco git checkout
 
 set -g async_prompt_functions _pure_prompt_git
-set -g pure_color_mute 696969
-set -g pure_color_hostname yellow
-set -g pure_color_git_branch magenta
-set -g pure_color_git_stash magenta
 
 set -x PAGER ~/.dotfiles/bin/pager
 set -x EDITOR ~/.dotfiles/bin/editor
