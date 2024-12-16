31e401f0a5476a3231b96bead8916d3f4aac3c0d
Adam C. Stephens <adam@valkor.net>
2024-12-16 09:38:01 -0500

    git-home: clean up failure handling

--- bin/dotlib/__init__.py
+++ bin/dotlib/__init__.py
@@ -1,15 +1,37 @@
-import subprocess, sys
+import os
+import shlex
+import subprocess
+import sys
 
 
 def run(command: str, capture_output: bool = True) -> str | None:
-    result = subprocess.run(command, shell=True, capture_output=capture_output)
+    run_command = shlex.split(command)
+
+    result = subprocess.run(run_command, capture_output=capture_output, encoding="UTF-8")
 
     if result.returncode != 0:
-        print(result.stderr.decode())
+        if capture_output and result.stderr is not None:
+            print(result.stderr)
         print(f"Failed to run [{command}]")
         sys.exit(1)
 
     if capture_output:
-        return result.stdout.decode().strip()
+        return result.stdout.strip()
 
     return None
+
+
+def find_git_up(chdir: bool = True):
+    dir = os.path.abspath(os.path.curdir)
+
+    while not os.path.exists(os.path.join(dir, ".git")):
+        if dir == os.getenv("HOME"):
+            print("Could not find git root in home directory")
+            sys.exit(1)
+
+        dir = os.path.dirname(dir)
+
+    if chdir:
+        os.chdir(dir)
+
+    return dir
