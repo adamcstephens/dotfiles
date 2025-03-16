fc3ee52dd6dbdc2e3e0f39532c5f2dbc92c363a7
Adam C. Stephens <adam@valkor.net>
2025-03-16 15:52:43 -0400

    sower: 0.4.2 -> 0.4.4

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     sandbox.url = "git+https://codeberg.org/adamcstephens/nix-sandbox";
     sandbox.inputs.nixpkgs.follows = "nixpkgs";
     sandbox.inputs.sower.follows = "sower";
-    sower.url = "git+https://codeberg.org/adamcstephens/sower?ref=refs/tags/v0.4.2";
+    sower.url = "git+https://codeberg.org/adamcstephens/sower?ref=refs/tags/v0.4.4";
   };
 
   outputs =
