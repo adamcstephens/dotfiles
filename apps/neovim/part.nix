b677abb05d9d0f6735f27628965ef958f608165c
Adam C. Stephens <adam@valkor.net>
2025-03-27 15:30:06 -0400

    neovim: bump to 0.11 from nixpkgs-unstable-small

--- apps/neovim/part.nix
+++ apps/neovim/part.nix
@@ -6,13 +6,16 @@
       ...
     }:
     let
-      pkgs = inputs'.nixpkgs-unstable.legacyPackages;
+      pkgs = inputs'.nixpkgs-unstable-small.legacyPackages;
     in
     {
       packages = rec {
         neovim = pkgs.callPackage ./package.nix {
           inherit (inputs) mnw;
           neovim = pkgs.neovim-unwrapped;
+          golangci-lint-langserver = pkgs.golangci-lint-langserver.overrideAttrs (_: {
+            doCheck = false;
+          });
         };
 
         neovim-full = neovim.override { full = true; };
