db48eddd95a5040c021012707beafc9a92f1d986
Adam C. Stephens <adam@valkor.net>
2025-06-04 23:33:25 -0400

    neovim: virtual line on current line only

--- apps/neovim/lua/dotinit/core.lua
+++ apps/neovim/lua/dotinit/core.lua
@@ -1,9 +1,9 @@
 vim.diagnostic.config({
   -- Use the default configuration
-  virtual_lines = true,
+  -- virtual_lines = true,
 
-  -- virtual_lines = {
-  --  -- Only show virtual line diagnostics for the current cursor line
-  --  current_line = true,
-  -- },
+  virtual_lines = {
+    -- Only show virtual line diagnostics for the current cursor line
+    current_line = true,
+  },
 })
