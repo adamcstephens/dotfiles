4d19324e53480a596a6d55e54f41ce531be0972d
Adam C. Stephens <adam@valkor.net>
2025-06-10 12:46:13 -0400

    vcs-tui: walk up to find repo

--- bin/vcs_tui.ml
+++ bin/vcs_tui.ml
@@ -1,3 +1,5 @@
+type vcs = Git | Jujutsu
+
 let well_known_socket =
   let base =
     match Sys.getenv_opt "XDG_RUNTIME_DIR" with
@@ -6,15 +8,30 @@ let well_known_socket =
   in
   base ^ "/ssh-auth.sock"
 
-let tui =
+let get_vcs dir =
   try
-    let _ = Sys.is_directory ".jj" in
-    Some "jjui"
+    let _ = Sys.is_directory @@ Filename.concat dir ".jj" in
+    Some Jujutsu
   with Sys_error _ -> (
     try
-      let _ = Sys.is_directory ".git" in
-      Some "lazygit"
+      let _ = Sys.is_directory @@ Filename.concat dir ".git" in
+      Some Git
     with Sys_error _ -> None)
+
+let rec walk_dir_up_f dir func =
+  if dir = Sys.getenv "HOME" || dir = "/" then None
+  else
+    match func dir with
+    | Some value -> Some value
+    | None -> walk_dir_up_f (Filename.dirname dir) func
+
+let find_vcs_up = walk_dir_up_f (Sys.getcwd ()) get_vcs
+
+let tui =
+  match find_vcs_up with
+  | Some Jujutsu -> Some "jjui"
+  | Some Git -> Some "lazygit"
+  | None -> None
 
 let () =
   match tui with
@@ -23,5 +40,5 @@ let () =
       Unix.putenv "SSH_AUTH_SOCK" well_known_socket;
       Unix.execvp tui_exec (Array.append [| tui_exec |] args)
   | None ->
-      Printf.eprintf "No VCS directory found";
+      Printf.eprintf "No VCS directory found\n";
       exit 1
