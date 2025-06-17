6b83a634bbbcc7cfbf90dd26efbace457500bacb
Adam C. Stephens <adam@valkor.net>
2025-06-17 00:00:08 -0400

    nvim: use default tiny-inline-diag theme

--- apps/neovim/lua/lazy/tiny-inline-diagnostic.lua
+++ apps/neovim/lua/lazy/tiny-inline-diagnostic.lua
@@ -5,8 +5,6 @@ return {
 
   after = function()
     -- https://github.com/rachartier/tiny-inline-diagnostic.nvim?tab=readme-ov-file#%EF%B8%8F-options
-    require("tiny-inline-diagnostic").setup({
-      preset = "classic",
-    })
+    require("tiny-inline-diagnostic").setup({})
   end,
 }
