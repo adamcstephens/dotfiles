e2ee1b8b467fa8a2a1e79bd98a40ca0ffc43b481
Adam C. Stephens <adam@valkor.net>
2025-08-14 08:18:29 -0400

    arkenfox: 135.0 -> 140.0
    Diff: https://github.com/arkenfox/user.js/compare/refs/tags/135.0...refs/tags/140.0

--- packages/arkenfox/default.nix
+++ packages/arkenfox/default.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "arkenfox";
-  version = "135.0";
+  version = "140.0";
 
   src = fetchFromGitHub {
     owner = "arkenfox";
     repo = "user.js";
     rev = "refs/tags/${version}";
-    hash = "sha256-21DoV4SMueMFRHMsvfsPfQIOtsvRWNY06rE4gB7xFnc=";
+    hash = "sha256-+DN5bKev9IyefMikOqEWZf/u0flJbLAxwEotGhKSSS4=";
   };
 
   dontBuild = true;
