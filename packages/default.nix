571d97cd8a8ba4e38b4ff039df73390ca13d2449
Adam C. Stephens <adam@valkor.net>
2025-08-15 09:56:02 -0400

    display-switch: init at 1.4.0

--- packages/default.nix
+++ packages/default.nix
@@ -14,6 +14,7 @@ in
 rec {
   arkenfox = pkgs.callPackage ./arkenfox { };
   default = dotfiles;
+  display-switch = pkgs.callPackage ./display-switch.nix { };
   dotfiles = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.callPackage ./dotfiles.nix {
     inherit ocamlPackages;
     static = pkgs.stdenv.isLinux && pkgs.stdenv.isx86_64;
---
+++ packages/display-switch.nix
@@ -0,0 +1,75 @@
+{
+  apple-sdk_15,
+  darwin,
+  fetchFromGitHub,
+  lib,
+  libgit2,
+  libusb1,
+  pkg-config,
+  rustPlatform,
+  stdenv,
+  udev,
+  versionCheckHook,
+  zlib,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "display-switch";
+  version = "1.4.0";
+
+  src = fetchFromGitHub {
+    owner = "haimgel";
+    repo = "display-switch";
+    tag = finalAttrs.version;
+    hash = "sha256-pUZNIEpzFZN5fc6TBedhL+7LJdw2R10w3BqzvLt+RYk=";
+  };
+
+  cargoHash = "sha256-AF6A880V4rZSxpfdnYNbXSi0Mz1Ig+ZOFMtI6pxKqeA=";
+
+  env = {
+    VERGEN_GIT_COMMIT_TIMESTAMP = "0";
+    VERGEN_GIT_BRANCH = "main";
+    VERGEN_GIT_DESCRIBE = finalAttrs.version;
+    VERGEN_GIT_SHA = finalAttrs.src.tag;
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [ darwin.DarwinTools ];
+
+  buildInputs = [
+    libgit2
+    libusb1
+    zlib
+  ]
+  ++ lib.optionals stdenv.isLinux [
+    udev
+  ]
+  ++ lib.optionals stdenv.isDarwin [
+    apple-sdk_15
+  ];
+
+  checkFlags = [
+    # TODO
+    "--skip=configuration::tests::test_log_file_name"
+    # actually tries to wake displays
+    "--skip=platform::wake_displays::tests::test_wake_displays"
+  ];
+
+  postInstall = ''
+    mv $out/bin/display_switch $out/bin/display-switch
+  '';
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+  versionCheckProgramArg = "--version";
+
+  meta = {
+    description = "Turn a $30 USB switch into a full-featured multi-monitor KVM switch";
+    homepage = "https://github.com/haimgel/display-switch";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ adamcstephens ];
+    mainProgram = "display-switch";
+  };
+})
