7c20e3f80f5b17bc69ac97ef98423552e5ee2174
Adam C. Stephens <adam@valkor.net>
2025-10-12 13:45:00 -0400

    Revert "wayland: waylock -> hyprlock, since waylock colors are busted"
    This reverts commit faf463ffde89b9ffe7160dc22bc6bb74d74b3946.

--- bin/wayland_locker.ml
+++ bin/wayland_locker.ml
@@ -1,9 +1,19 @@
+open Yojson.Basic.Util
+open Dotfiles.Colors
 open Dotfiles.Desktop
 open Dotfiles.Process
 
+let waylock ~palette =
+  let init_color = palette |> member "base04" |> to_string in
+  let input_color = palette |> member "base0A" |> to_string in
+  let fail_color = palette |> member "base08" |> to_string in
+  Printf.sprintf
+    "waylock -fork-on-lock -init-color 0x%s -input-color 0x%s -fail-color 0x%s"
+    init_color input_color fail_color
+
 let locker desktop =
   match desktop with
-  | River -> "gtklock --daemonize"
+  | River -> waylock ~palette:load_colors
   | Niri -> "gtklock --daemonize"
 
 let do_lock ~cmd =
