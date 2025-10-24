97ea1b2891e07a28beee06945cb0768f6d25bea2
Adam C. Stephens <adam@valkor.net>
2025-10-24 08:56:07 -0400

    fish: hardcode word/token bindings to avoid platform dependence

--- apps/fish/interactive.fish
+++ apps/fish/interactive.fish
@@ -11,3 +11,15 @@ abbr --add gco git checkout
 
 set -x PAGER ~/.dotfiles/bin/pager
 set -x EDITOR ~/.dotfiles/bin/editor
+
+# fish made word/token platform dependent, so we'll hardcode these
+bind alt-backspace backward-kill-word
+bind alt-delete kill-word
+bind alt-left prevd-or-backward-word
+bind alt-right nextd-or-forward-word
+bind ctrl-alt-h backward-kill-word
+bind ctrl-backspace backward-kill-token
+bind ctrl-delete kill-token
+bind ctrl-h backward-kill-token
+bind ctrl-left backward-token
+bind ctrl-right forward-token
