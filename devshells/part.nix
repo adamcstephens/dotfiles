f72f05fa19b7917772afe625de11661fdd101a29
Adam C. Stephens <adam@valkor.net>
2025-11-20 19:36:51 -0500

    devshells: only use lix on linux

--- devshells/part.nix
+++ devshells/part.nix
@@ -14,55 +14,60 @@
 
   perSystem =
     {
+      inputs',
       pkgs-unstable,
       system,
       ...
     }:
     let
-      # use unstable for devshells
-      pkgs = pkgs-unstable;
-
       npins = import ../npins;
     in
     {
-      _module.args = {
-        inherit npins;
+      _module.args =
+        if (system == "x86_64-linux") then
+          {
+            inherit npins;
 
-        # lix overlay for flake-parts
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            (import "${npins.lix-nixos-module}/overlay.nix" {
-              # use nixpkgs lix
-              lix = npins.lix;
-            })
-          ];
-        };
-        pkgs-unstable = import inputs.nixpkgs-unstable {
-          inherit system;
-          overlays = [
-            (import "${npins.lix-nixos-module}/overlay.nix" {
-              # use nixpkgs lix
-              lix = npins.lix;
-            })
-          ];
-        };
-      };
+            # lix overlay for flake-parts
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                (import "${npins.lix-nixos-module}/overlay.nix" {
+                  # use nixpkgs lix
+                  lix = npins.lix;
+                })
+              ];
+            };
+            pkgs-unstable = import inputs.nixpkgs-unstable {
+              inherit system;
+              overlays = [
+                (import "${npins.lix-nixos-module}/overlay.nix" {
+                  # use nixpkgs lix
+                  lix = npins.lix;
+                })
+              ];
+            };
+          }
+        else
+          {
+            inherit npins;
+            pkgs-unstable = inputs'.nixpkgs-unstable.legacyPackages;
+          };
 
       devShells = {
-        cd = pkgs.callPackage ./cd.nix { };
-        ci = pkgs.callPackage ./ci.nix { inherit inputs; };
-        default = pkgs.callPackage ./default.nix { };
+        cd = pkgs-unstable.callPackage ./cd.nix { };
+        ci = pkgs-unstable.callPackage ./ci.nix { inherit inputs; };
+        default = pkgs-unstable.callPackage ./default.nix { };
 
-        c = pkgs.callPackage ./c.nix { };
-        elixir = pkgs.callPackage ./elixir.nix { };
-        go = pkgs.callPackage ./go.nix { };
-        js = pkgs.callPackage ./js.nix { };
-        nixpkgs = pkgs.callPackage ./nixpkgs.nix { };
-        ocaml = pkgs.callPackage ./ocaml.nix { inherit inputs; };
-        python = pkgs.callPackage ./python.nix { };
-        rust = pkgs.callPackage ./rust.nix { };
-        zig = pkgs.callPackage ./zig.nix { };
+        c = pkgs-unstable.callPackage ./c.nix { };
+        elixir = pkgs-unstable.callPackage ./elixir.nix { };
+        go = pkgs-unstable.callPackage ./go.nix { };
+        js = pkgs-unstable.callPackage ./js.nix { };
+        nixpkgs = pkgs-unstable.callPackage ./nixpkgs.nix { };
+        ocaml = pkgs-unstable.callPackage ./ocaml.nix { inherit inputs; };
+        python = pkgs-unstable.callPackage ./python.nix { };
+        rust = pkgs-unstable.callPackage ./rust.nix { };
+        zig = pkgs-unstable.callPackage ./zig.nix { };
       };
     };
 }
