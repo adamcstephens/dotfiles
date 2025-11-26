ab8225e3471b20a5671e059138b031f35d636386
Adam C. Stephens <adam@valkor.net>
2025-11-26 09:40:36 -0500

    nvim: disable osc52 paste

--- apps/neovim/lua/dotinit.lua
+++ apps/neovim/lua/dotinit.lua
@@ -26,10 +26,10 @@ vim.g.clipboard = {
     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
   },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
+  -- paste = {
+  --   ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+  --   ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  -- },
 }
 
 -- disable end of line diagnostic message
