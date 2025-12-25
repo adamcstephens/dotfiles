5b203095e2a67f3d92fc1f650309a6442ddf745e
Adam C. Stephens <adam@valkor.net>
2025-12-25 14:42:01 -0500

    fish: only reset smart splits if in tmux

--- apps/fish/functions/_smart-splits.fish
+++ apps/fish/functions/_smart-splits.fish
@@ -1,5 +1,7 @@
 # work around smart-splits not clearing pane status
 # we rarely use a neovim terminal anyway
 function _smart-splits --on-event="fish_preexec"
-    tmux set -p @pane-is-vim ''
+    if test -n "$TMUX"
+        tmux set -p @pane-is-vim ''
+    end
 end
