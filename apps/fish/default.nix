7774d25a945bd5bb6f57f31f13df2bf633e32ee8
Adam C. Stephens <adam@valkor.net>
2026-01-11 00:16:28 -0500

    fish: use function for links

--- apps/fish/default.nix
+++ apps/fish/default.nix
@@ -14,6 +14,13 @@ let
     echo "end"
     echo "setup_hm_session_vars") > $out/share/fish/vendor_conf.d/hm-session-vars.fish
   '';
+
+  mkSource =
+    source:
+    if config.dotfiles.nixosManaged then
+      ./. + "/${source}"
+    else
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/fish/${source}";
 in
 {
   home.packages = [
@@ -21,18 +28,11 @@ in
     homeManagerSessionVariables
   ];
 
-  xdg.configFile = lib.genAttrs' [ "completions" "conf.d" "config.fish" "functions" ] (
-    source:
-    lib.nameValuePair "fish/${source}" {
-      source =
-        if config.dotfiles.nixosManaged then
-          ./. + "/${source}"
-        else
-          config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/apps/fish/${source}";
-    }
-  );
-
   # xdg.configFile."fish/theme-dark.fish".source = npins.vim-moonfly-colors + "/extras/moonfly.fish";
   # xdg.configFile."fish/theme-light.fish".source =
   #   npins."modus-themes.nvim" + "/extras/fish/modus_operandi.fish";
+  xdg.configFile."fish/completions".source = mkSource "completions";
+  xdg.configFile."fish/conf.d".source = mkSource "conf.d";
+  xdg.configFile."fish/config.fish".source = mkSource "config.fish";
+  xdg.configFile."fish/functions".source = mkSource "functions";
 }
