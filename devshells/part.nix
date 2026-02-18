57f31d8d98e8bbf9ec5e7b5e8ae258e043c8bf19
Adam C. Stephens <adam@valkor.net>
2026-02-17 22:32:26 -0500

    devshells/dotfiles: rename from default

--- devshells/part.nix
+++ devshells/part.nix
@@ -55,9 +55,10 @@
           };
 
       devShells = {
+        dotfiles = pkgs-unstable.callPackage ./dotfiles.nix { inherit inputs; };
+
         cd = pkgs-unstable.callPackage ./cd.nix { };
         ci = pkgs-unstable.callPackage ./ci.nix { inherit inputs; };
-        default = pkgs-unstable.callPackage ./default.nix { inherit inputs; };
 
         c = pkgs-unstable.callPackage ./c.nix { };
         elixir = pkgs-unstable.callPackage ./elixir.nix { };
