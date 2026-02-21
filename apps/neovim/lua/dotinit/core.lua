e70813f9eac8db39c4d5097e50afab0c63bd7660
Adam C. Stephens <adam@valkor.net>
2026-02-20 23:34:41 -0500

    nvim: drop deprecated calls that weren't doing anything?

--- apps/neovim/lua/dotinit/core.lua
+++ apps/neovim/lua/dotinit/core.lua
@@ -1,16 +1,5 @@
 vim.diagnostic.config({
-  -- Use the default configuration
-  -- virtual_lines = true,
-
-  -- virtual_lines = {
-  --   -- Only show virtual line diagnostics for the current cursor line
-  --   current_line = true,
-  -- },
-
   float = {
     border = "rounded",
   },
 })
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
