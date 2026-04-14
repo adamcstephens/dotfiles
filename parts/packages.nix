5dff8216db4ecc692b5ec471c1f10c2c11a0d449
Adam C. Stephens <adam@valkor.net>
2026-04-14 09:08:11 -0400

    darwin: move packages to own subdir

--- parts/packages.nix
+++ parts/packages.nix
@@ -25,6 +25,12 @@
           inherit (pkgs) callPackage;
           directory = ../packages;
         }
+        // lib.optionalAttrs pkgs.stdenv.isDarwin (
+          lib.filesystem.packagesFromDirectoryRecursive {
+            inherit (pkgs) callPackage;
+            directory = ../packages-darwin;
+          }
+        )
         // {
           default = self'.packages.dotfiles;
 
