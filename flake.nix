6ea9745cc4acc58c7a605b0c1da7078a825c5a7d
Adam C. Stephens <adam@valkor.net>
2026-05-30 09:13:12 -0400

    nixpkgs: 25.11 -> 26.05

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-unstable-small.url = "github:nixos/nixpkgs?ref=nixos-unstable-small";
 
-    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
+    home-manager.url = "github:nix-community/home-manager?ref=release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager-unstable.url = "github:nix-community/home-manager";
     home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs-unstable";
