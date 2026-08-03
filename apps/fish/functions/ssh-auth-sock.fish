db79bbce10b128c44dab11c88a2613ecb3e6089b
Adam C. Stephens <adam@valkor.net>
2026-08-03 11:19:58 -0400

    fish/ssh-auth-sock: use short args for ln for darwin compat

--- apps/fish/functions/ssh-auth-sock.fish
+++ apps/fish/functions/ssh-auth-sock.fish
@@ -12,7 +12,7 @@ function ssh-auth-sock --on-event="fish_preexec"
 
     if [ -z "$TMUX" ] && [ -z "$ZELLIJ" ]
         if [ "$SSH_AUTH_SOCK" != "(readlink $sockfile)" ] && [ "$SSH_AUTH_SOCK" != "$sockfile" ]
-            ln --symbolic --force $SSH_AUTH_SOCK $sockfile
+            ln -sf $SSH_AUTH_SOCK $sockfile
             ssh-add -L | head --lines=1 >~/.ssh/signing-key.pub
         end
     else
