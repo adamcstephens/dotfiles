2549b1f919d168fe45d085bc6749abcb323a5117
Adam C. Stephens <adam@valkor.net>
2026-08-04 13:26:11 -0400

    fish: expose cnf as its own command

--- apps/fish/default.nix
+++ apps/fish/default.nix
@@ -25,26 +25,24 @@ let
     else
       "${config.directory}/.dotfiles/apps/fish/${source}";
 
+  commandNotFound = pkgs.writeScript "fish-command-not-found" ''
+    #!${pkgs.bash}/bin/bash
+    source ${
+      inputs.nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-small-db
+    }/etc/profile.d/command-not-found.sh
+    command_not_found_handle "$@"
+  '';
+
   # we'll steal this from HM
-  commandNotFound =
-    let
-      wrapper = pkgs.writeScript "fish-command-not-found" ''
-        #!${pkgs.bash}/bin/bash
-        source ${
-          inputs.nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-small-db
-        }/etc/profile.d/command-not-found.sh
-        command_not_found_handle "$@"
-      '';
-    in
-    pkgs.writeTextFile {
-      name = "fish-command-not-found";
-      text = ''
-        function __fish_command_not_found_handler --on-event fish_command_not_found
-            ${wrapper} $argv
-        end
-      '';
-      destination = "/share/fish/vendor_functions.d/__fish_command_not_found_handler.fish";
-    };
+  fishCommandNotFound = pkgs.writeTextFile {
+    name = "fish-command-not-found";
+    text = ''
+      function __fish_command_not_found_handler --on-event fish_command_not_found
+          ${commandNotFound} $argv
+      end
+    '';
+    destination = "/share/fish/vendor_functions.d/__fish_command_not_found_handler.fish";
+  };
 
   themeDump =
     category: source:
@@ -71,6 +69,7 @@ in
     package
     hjemSessionVariables
     commandNotFound
+    fishCommandNotFound
   ];
 
   xdg.config.files."fish/completions".source = mkSource "completions";
