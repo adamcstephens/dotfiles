94d775999be64a22c9bfa186929a615a92c80759
Adam C. Stephens <adam@valkor.net>
2026-08-10 20:26:37 -0400

    noctalia: bump to beta.8

--- apps/noctalia/default.nix
+++ apps/noctalia/default.nix
@@ -1,62 +1,74 @@
 {
   config,
+  inputs,
   lib,
   pkgs,
   profile,
   ...
 }:
 let
-  package = pkgs.noctalia.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      ./sorted-workspaces.patch
-      ./add-pinnacle.patch
-      ./logind-idle-hint.patch
-      ./inhibit-sync.patch
-    ];
-  });
+  cfg = config.apps.noctalia;
 in
 {
-  xdg.config.files = {
-    noctalia.source =
-      if config.dotfiles.nixosManaged then ./. else "${config.directory}/.dotfiles/apps/noctalia";
-  }
-  // lib.optionalAttrs (builtins.pathExists (./profiles + "/${profile}.toml")) {
-    "noctalia/profile.toml".text = # toml
-      ''
-        [include]
-        files = ["profiles/${profile}.toml"]
-      '';
+  options = {
+    dotfiles.apps.noctalia.package = lib.mkOption {
+      type = lib.types.package;
+      default =
+        inputs.nixos-unstable-small.legacyPackages.${pkgs.stdenv.hostPlatform.system}.noctalia.overrideAttrs
+          (old: {
+            patches = (old.patches or [ ]) ++ [
+              ./sorted-workspaces.patch
+              ./add-pinnacle.patch
+              ./logind-idle-hint.patch
+              ./inhibit-sync.patch
+            ];
+          });
+    };
   };
 
-  packages = [
-    package
-  ];
-
-  systemd.services.noctalia = {
-    partOf = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    wantedBy = [ "graphical-session.target" ];
-
-    enableDefaultPath = false;
-
-    environment = {
-      XDG_DATA_DIRS = lib.concatStringsSep ":" [
-        "${config.directory}/.local/state/hjem/standalone/current-profile/share"
-        "${config.directory}/.local/state/nix/profile/share"
-        "${config.directory}/.nix-profile/share"
-        "${config.directory}/.local/share/flatpak/exports/share"
-        "/run/current-system/sw/share"
-      ];
+  config = {
+    xdg.config.files = {
+      noctalia.source =
+        if config.dotfiles.nixosManaged then ./. else "${config.directory}/.dotfiles/apps/noctalia";
+    }
+    // lib.optionalAttrs (builtins.pathExists (./profiles + "/${profile}.toml")) {
+      "noctalia/profile.toml".text = # toml
+        ''
+          [include]
+          files = ["profiles/${profile}.toml"]
+        '';
     };
 
-    path = [
-      "${config.directory}/.local/state/hjem/standalone/current-profile"
-      "/run/current-system/sw"
+    packages = [
+      cfg.package
     ];
 
-    serviceConfig = {
-      ExecStart = lib.getExe package;
-      Restart = "on-failure";
+    systemd.services.noctalia = {
+      partOf = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+
+      enableDefaultPath = false;
+
+      environment = {
+        XDG_DATA_DIRS = lib.concatStringsSep ":" [
+          "${config.directory}/.local/state/hjem/standalone/current-profile/share"
+          "${config.directory}/.local/state/nix/profile/share"
+          "${config.directory}/.nix-profile/share"
+          "${config.directory}/.local/share/flatpak/exports/share"
+          "/run/current-system/sw/share"
+        ];
+      };
+
+      path = [
+        "${config.directory}/.local/state/hjem/standalone/current-profile"
+        "/run/current-system/sw"
+      ];
+
+      serviceConfig = {
+        ExecStart = lib.getExe cfg.package;
+        Restart = "on-failure";
+      };
     };
   };
 }
