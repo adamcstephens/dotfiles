8bd190609a1c9fa1b99a6bd622b48a8ed997a0b8
Adam C. Stephens <adam@valkor.net>
2026-08-11 00:05:30 -0400

    pinnacle: fix luarc load path

--- apps/pinnacle/default.nix
+++ apps/pinnacle/default.nix
@@ -1,5 +1,10 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-  xdg.config.files."pinnacle".source =
-    if config.dotfiles.nixosManaged then ./. else "${config.directory}/.dotfiles/apps/pinnacle";
+  xdg.config.files = {
+    "pinnacle".source =
+      if config.dotfiles.nixosManaged then ./. else "${config.directory}/.dotfiles/apps/pinnacle";
+
+    # won't match the system version, but should be close enough
+    "pinnacle/share".source = "${pkgs.pinnacle.lua-client-api}/share";
+  };
 }
