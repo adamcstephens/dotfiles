7af4ff660c5292217046b34bba0776d354d19e30
Adam C. Stephens <adam@valkor.net>
2026-08-27 21:50:45 -0400

    zellij: fix nixosManaged again

--- apps/zellij/default.nix
+++ apps/zellij/default.nix
@@ -5,7 +5,11 @@
   ...
 }:
 let
-  package = pkgs.zellij;
+  package = pkgs.zellij.override {
+    extraPackages = [
+      pkgs.zellijPlugins.vim-zellij-navigator
+    ];
+  };
 in
 {
   packages = [
@@ -13,21 +17,18 @@ in
     package
   ];
 
+  xdg.config.files."zellij/layouts".source =
+    if config.dotfiles.nixosManaged then
+      ./layouts
+    else
+      "${config.directory}/.dotfiles/apps/zellij/layouts";
+
   xdg.config.files."zellij/config.kdl".source =
     if config.dotfiles.nixosManaged then
       ./config.kdl
     else
       "${config.directory}/.dotfiles/apps/zellij/config.kdl";
 
-  xdg.config.files."zellij/themes".source =
-    if config.dotfiles.nixosManaged then
-      ./themes
-    else
-      "${config.directory}/.dotfiles/apps/zellij/themes";
-
   xdg.config.files."zellij/themes/moonfly.kdl".source =
     npins.vim-moonfly-colors + "/extras/moonfly-zellij.kdl";
-
-  xdg.config.files."zellij/plugins/vim-zellij-navigator.wasm".source =
-    pkgs.zellijPlugins.vim-zellij-navigator;
 }
