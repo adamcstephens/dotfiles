b7cf4fa06af8823b12679316817d8f9bac6c7d59
Adam C. Stephens <adam@valkor.net>
2026-09-16 12:45:33 -0400

    nono-claude: add sso login if needed

--- bin/nono-claude.sh
+++ bin/nono-claude.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+aws sts get-caller-identity &>/dev/null || aws sso login
+
 eval $(aws configure export-credentials --profile bitfreighter-readonlyaccess --format env)
 
 export AWS_DEFAULT_REGION=us-east-2
